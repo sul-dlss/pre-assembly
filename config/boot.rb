e835c7602d52f4ee03aef551f3de15a28a76d87a
Monty Hindman <montyhindman@gmail.com>
2012-02-10 14:52:00 -0800

    Adjusted how PATH_TO_GEM was defined: using a relative path.

--- config/boot.rb
+++ config/boot.rb
@@ -4,5 +4,4 @@ ASSEMBLY_ROOT = File.expand_path(File.dirname(__FILE__) + '/..')
 
 $LOAD_PATH.unshift File.expand_path(ASSEMBLY_ROOT + '/lib')
 
-# Recursively require all .rb files in the lib directory.
-Dir["#{ASSEMBLY_ROOT}/lib/**/*.rb"].sort.each { |f| require f }
+require 'assembly'
--- lib/assembly.rb
+++ lib/assembly.rb
@@ -6,7 +6,9 @@
 # Author::    SULAIR DLSS
 # see README for prerequisites
 
+require 'assembly/images'
+require 'assembly/version'
+
 module Assembly
-  # TODO: replace PATH_TO_GEM usages.
-  # PATH_TO_GEM = Gem.loaded_specs['assembly'].full_gem_path
+  PATH_TO_GEM = File.expand_path(File.dirname(__FILE__) + '/..')
 end
