cdaa51d100551bc04faca7b664c3199fda696445
Monty Hindman <montyhindman@gmail.com>
2012-02-21 14:39:30 -0800

    Wrote code to register objects, optionally deleting them (during development).

--- lib/assembly/digital_object.rb
+++ lib/assembly/digital_object.rb
@@ -7,13 +7,16 @@ module Assembly
     attr_accessor :source_id, :already_processed, :druid, :images
 
     def initialize(source_id = '')
-      @source_id         = source_id
       @already_processed = false
-      @druid             = ''
+      @druid             = nil
       @images            = []
-      @project_name      = 'REVS'              # TODO
-      @apo_druid_id      = 'druid:qv648vd4392' # TODO
-      @collection        = 'druid:nt028fd5773' # TODO
+
+      # TODO: modify these hard-coded, REVS-specific values.
+      @project_name      = 'revs'
+      @apo_druid_id      = 'druid:qv648vd4392'
+      @collection        = 'druid:nt028fd5773'
+      @source_id         = {  @project_name => source_id }
+      @source_id_key     = "#{@project_name}:#{source_id}"
     end
 
     def add_image(file_name)
@@ -21,35 +24,28 @@ module Assembly
     end
 
     def register
-      # See SURI 2.0 confluence page for how to get a batch of druids in one call.
-      # 
-      #   curl -X POST 
-      #        http://***REMOVED***@lyberservices-dev.stanford.edu:8080
-      #        /suri2/namespaces/druid/identifiers?quantity=99
-      log "    - register()"
+      pid    = Dor::SuriService.mint_id
+      @druid = Druid.new pid
 
-      pid   = Dor::SuriService.mint_id
-      druid = Druid.new pid
+      log "    - register(#{@druid.id})"
+      
       params = {
         :object_type  => 'item',
         :admin_policy => @apo_druid_id,
         :source_id    => @source_id,
-        :label        => "#{@project_name}_#{druid.id}",
-        :tags         => ["Project : #{@project_name}"]
+        :pid          => pid,
+        :label        => "#{@project_name}_#{@druid.id}",
+        :tags         => ["Project : #{@project_name}"],
+        :other_ids    => { 'uuid' => UUIDTools::UUID.timestamp_create.to_s },
       }
 
-      p params
-      exit
+      Dor::RegistrationService.register_object(params)
+    end
 
-      result = Dor::RegistrationService.register_object(params)
-      p result
-      exit
-      # Returns:
-      # result = {
-      #   :response => http_response,
-      #    :pid => pid
-      # }
-      
+    def nuke
+      d = "druid:#{@druid.id}"
+      log "    - nuke(#{d})"
+      Dor::Config.fedora.client["objects/#{d}"].delete
     end
 
   end
