6bbc3f280a7e1605a55769d1ec272b5c41b0f0a4
Monty Hindman <montyhindman@gmail.com>
2012-02-29 15:15:16 -0800

    Modified spec_helper to use development environment for now.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
-environment = ENV['ROBOT_ENVIRONMENT'] = 'test'
+# TODO: sort out environment issues; current set to development.
+environment = ENV['ROBOT_ENVIRONMENT'] ||= 'development'
 
 bootfile = File.expand_path(File.dirname(__FILE__) + '/../config/boot')
 require bootfile
