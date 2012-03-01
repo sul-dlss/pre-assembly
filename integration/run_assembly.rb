230f0642d491ae0c7c2710364de18449d0fd4711
Monty Hindman <montyhindman@gmail.com>
2012-02-29 16:21:46 -0800

    Started setting up bin/run_assembly as an integration test under RSpec.
    Also required some config changes:
    - Dependency: lyber-core ~> 1.3.0.
    - In boot file: require 'bundler/setup'
    - Made a temporary hack-fix to lyber-core:
        cd `bundle show lyber-core`
        rm -rf lib/xml_models
        cd -

---
+++ integration/run_assembly.rb
@@ -0,0 +1,21 @@
+describe "Pre-assembly integration" do
+
+  before(:each) do
+    @bundle = Assembly::Bundle.new(
+      :bundle_dir          => 'spec/test_data/bundle_input',
+      :manifest            => 'manifest.csv',
+      :checksums_file      => 'checksums.txt',
+      :project_name        => 'REVS',
+      :apo_druid_id        => 'druid:qv648vd4392',
+      :collection_druid_id => 'druid:nt028fd5773',
+      :staging_dir         => 'tmp',
+      :copy_to_staging     => true,
+      :cleanup             => true
+    )
+  end
+
+  it "can run the assembly process" do
+    @bundle.run_assembly
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 # TODO: sort out environment issues; current set to development.
+# TODO: require spec_helper.rb explicitly rather than using .rspec file?
 environment = ENV['ROBOT_ENVIRONMENT'] ||= 'development'
 
 bootfile = File.expand_path(File.dirname(__FILE__) + '/../config/boot')
