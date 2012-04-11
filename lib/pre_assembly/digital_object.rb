f3f4b454a69dc46371c110af22686cb60ba511d0
Monty Hindman <montyhindman@gmail.com>
2012-04-11 16:31:13 -0700

    Added @reg_by_pre_assembly to DigitalObject.

--- lib/pre_assembly/digital_object.rb
+++ lib/pre_assembly/digital_object.rb
@@ -9,6 +9,7 @@ module PreAssembly
       :stageable_items,
       :object_files,
       :manifest_attr,
+      :reg_by_pre_assembly,
       :project_name,
       :apo_druid_id,
       :set_druid_id,
@@ -38,6 +39,7 @@ module PreAssembly
       @stageable_items            = params[:stageable_items]
       @object_files               = params[:object_files]
       @manifest_attr              = params[:manifest_attr]
+      @reg_by_pre_assembly        = false
       @project_name               = params[:project_name]
       @apo_druid_id               = params[:apo_druid_id]
       @set_druid_id               = params[:set_druid_id]
