0732835aaeb5381e7052955a3b37504701d353ae
Monty Hindman <montyhindman@gmail.com>
2012-04-13 15:36:17 -0700

    Integration tests: refactored to check for files in particular druid trees.
    - Also modified run_pre_assemble to return a list of pids.

--- integration/run_preassembly_spec.rb
+++ integration/run_preassembly_spec.rb
@@ -1,8 +1,7 @@
 describe "Pre-assembly integration" do
 
-  def setup_bundle(project_style)
+  def setup_bundle(proj)
     # Load the project's YAML config file.
-    proj      = project_style.to_s.sub /^style_/, ''
     yaml_file = "#{PRE_ASSEMBLY_ROOT}/config/projects/local_dev_#{proj}.yaml"
     yaml      = YAML.load_file yaml_file
     @params   = PreAssembly::Bundle.symbolize_keys yaml
@@ -18,38 +17,54 @@ describe "Pre-assembly integration" do
     @b = PreAssembly::Bundle.new @params
 
     # Set values needed for assertions.
-    conf         = Dor::Config.pre_assembly
-    dru_tree     = "#{@temp_dir}/??/???/??/????"
-    @exp_n_files = 3
-    @exp_files   = [
-      "#{dru_tree}/*.tif",
-      "#{dru_tree}/#{conf.cm_file_name}",
-      "#{dru_tree}/#{conf.dm_file_name}",
-    ]
+    conf       = Dor::Config.pre_assembly
+    dru_tree   = "#{@temp_dir}/??/???/??/????"
+    @n_objects = 3
+    @exp_files = ['*.tif', conf.cm_file_name, conf.dm_file_name]
   end
 
   def check_for_expected_files
-    @exp_files.each do |glob_pattern|
-      fs = Dir[glob_pattern].select { |f| File.file? f }
-      fs.size.should == @exp_n_files
+    # Get pids of the process objects and determine the druid tree paths
+    # in the staging directory.
+    pids        = @b.run_pre_assembly
+    druid_trees = pids.map { |pid| Druid.new(pid).path(@temp_dir) }
+
+    # Did we get the expect N of staged objects?
+    druid_trees.size.should == @n_objects
+
+    # Make sure the files were staged as we expected.
+    druid_trees.each do |dt|
+      @exp_files.each do |ef|
+        glob = File.join dt, ef
+        fs   = Dir[glob]
+        fs.size.should == 1
+      end
     end
+
   end
 
-  describe "Revs-style project" do
+  describe "Revs" do
 
     it "should produce expected files in staging dir" do
-      setup_bundle :style_revs
-      @b.run_pre_assembly
+      setup_bundle 'revs'
       check_for_expected_files
     end
 
   end
 
-  describe "Rumsey-style project" do
+  describe "Rumsey" do
 
     it "should produce expected files in staging dir" do
-      setup_bundle :style_rumsey
-      @b.run_pre_assembly
+      setup_bundle 'rumsey'
+      check_for_expected_files
+    end
+
+  end
+
+  describe "ReidDennis" do
+
+    it "should produce expected files in staging dir" do
+      setup_bundle 'reid_dennis'
       check_for_expected_files
     end
 
