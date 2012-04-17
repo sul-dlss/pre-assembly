730c424aacc24b5ac58ee90f0ef19119e2a1b48c
Monty Hindman <montyhindman@gmail.com>
2012-04-17 14:38:31 -0700

    Finished tests for DruidMinter.

--- spec/druid_minter_spec.rb
+++ spec/druid_minter_spec.rb
@@ -1,14 +1,18 @@
 describe PreAssembly::DruidMinter do
 
   before(:all) do
-    @exp = String.new PreAssembly::DruidMinter.current
+    @minter = PreAssembly::DruidMinter
   end
 
-  it "can exercise next()" do
-    5.times do
-      v = PreAssembly::DruidMinter.next
-      # v.should == @exp
-    end
+  it "should get a sequence of druids from calls to next()" do
+    exp = @minter.current
+    3.times { @minter.next.should == exp.next! }
+  end
+
+  it "should return unique string objects" do
+    o1 = @minter.next
+    o2 = @minter.next
+    o1.object_id.should_not == o2.object_id
   end
 
 end
