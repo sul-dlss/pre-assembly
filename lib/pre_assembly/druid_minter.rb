510ffbac2f9ce20bf9547789ada1c76c4fc8492c
Monty Hindman <montyhindman@gmail.com>
2012-04-17 14:38:31 -0700

    Finished tests for DruidMinter.

--- lib/pre_assembly/druid_minter.rb
+++ lib/pre_assembly/druid_minter.rb
@@ -2,14 +2,19 @@ module PreAssembly
 
   class DruidMinter
 
+    # A class to return new druids.
+    # Used during integration tests when we need a druid but cannot
+    # call the correct Dor service.
+
     @@druid = 'druid:aa000bb0000'
 
     def self.current
-      @@druid
+      return String.new(@@druid)
     end
 
     def self.next
-      String.new @@druid.next!
+      @@druid.next!
+      self.current
     end
 
   end
