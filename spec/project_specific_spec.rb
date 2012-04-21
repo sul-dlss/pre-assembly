11b3b638ed47ef93fdbd86ba333cad11c9dae35c
Peter Mangiafico <peter@mangiafico.org>
2012-04-20 17:40:47 -0700

    create passing test for smpl content metadata generation

--- spec/project_specific_spec.rb
+++ spec/project_specific_spec.rb
@@ -10,12 +10,15 @@ describe PreAssembly::DigitalObject do
       :publish_attr  => { :publish => 'no', :shelve => 'no', :preserve => 'yes' },
       :project_style => {},
       :bundle_dir    => 'spec/test_data/bundle_input_e',
+      :content_md_creation => {:style=>:smpl,:pre_md_file=>'preContentMetadata.xml'}
     }
     @dobj         = PreAssembly::DigitalObject.new @ps
     @dru          = 'aa111aa1111'
     @pid          = "druid:#{@dru}"
     @druid        = Druid.new @pid
     @tmp_dir_args = [nil, 'tmp']
+    @dobj.druid = @druid
+    @dobj.container = @dru    
   end
 
 
@@ -24,26 +27,50 @@ describe PreAssembly::DigitalObject do
   describe "SMPL specific methods in project_specific module" do
 
     it "should convert SMPL content metadata into valid base content metadata" do
-      @dobj.druid = @druid
-      @dobj.create_content_metadata_xml_smpl
+
       @exp_xml = <<-END.gsub(/^ {8}/, '')
-        <?xml version="1.0"?>
-        <contentMetadata objectId="ab123cd4567">
-          <resource sequence="1" id="ab123cd4567_1">
-            <label>Item 1</label>
-            <file preserve="yes" publish="no" shelve="no" id="image_1.tif">
-              <provider_checksum type="md5">1111</provider_checksum>
-            </file>
-          </resource>
-          <resource sequence="2" id="ab123cd4567_2">
-            <label>Item 2</label>
-            <file preserve="yes" publish="no" shelve="no" id="image_2.tif">
-              <provider_checksum type="md5">2222</provider_checksum>
-            </file>
-          </resource>
-        </contentMetadata>
+      <?xml version="1.0"?>
+      <contentMetadata objectId="aa111aa1111" type="file">
+        <resource sequence="1" id="aa111aa1111_1" type="file">
+          <label>Tape 1, Side A</label>
+          <file shelve="no" publish="yes" preserve="yes" id="PM/aa111aa1111_001_a_pm.wav">
+            <provider_checksum type="md5">checksumforaa111aa1111_001_a_pm.wav</provider_checksum>
+          </file>
+          <file shelve="no" publish="yes" preserve="yes" id="SH/aa111aa1111_001_a_sh.wav">
+            <provider_checksum type="md5">checksumforaa111aa1111_001_a_sh.wav</provider_checksum>
+          </file>
+          <file shelve="no" publish="yes" preserve="yes" id="SL/aa111aa1111_001_a_sl.mp3">
+            <provider_checksum type="md5">checksumforaa111aa1111_001_a_sl.mp3</provider_checksum>
+          </file>
+          <file shelve="yes" publish="yes" preserve="yes" id="Images/aa111aa1111_001_img_1.jpg">
+            <provider_checksum type="md5">checksumforaa111aa1111_001_img_1.jpg</provider_checksum>
+          </file>
+        </resource>
+        <resource sequence="2" id="aa111aa1111_2" type="file">
+          <label>Tape 1, Side B</label>
+          <file shelve="no" publish="yes" preserve="yes" id="PM/aa111aa1111_001_b_pm.wav">
+            <provider_checksum type="md5">checksumforaa111aa1111_001_b_pm.wav</provider_checksum>
+          </file>
+          <file shelve="no" publish="yes" preserve="yes" id="SH/aa111aa1111_001_b_sh.wav">
+            <provider_checksum type="md5">checksumforaa111aa1111_001_b_sh.wav</provider_checksum>
+          </file>
+          <file shelve="no" publish="yes" preserve="yes" id="SL/aa111aa1111_001_b_sl.mp3">
+            <provider_checksum type="md5">checksumforaa111aa1111_001_b_sl.mp3</provider_checksum>
+          </file>
+          <file shelve="yes" publish="yes" preserve="yes" id="Images/aa111aa1111_001_img_2.jpg">
+            <provider_checksum type="md5">checksumforaa111aa1111_001_img_2.jpg</provider_checksum>
+          </file>
+        </resource>
+        <resource sequence="3" id="aa111aa1111_3" type="file">
+          <label>Transcript</label>
+          <file shelve="yes" publish="yes" preserve="yes" id="Transcript/aa111aa1111.pdf">
+            <provider_checksum type="md5">checksumforedited_transcript.pdf</provider_checksum>
+          </file>
+        </resource>
+      </contentMetadata>
       END
       @exp_xml = noko_doc @exp_xml
+      noko_doc(@dobj.create_content_metadata_xml_smpl).should be_equivalent_to @exp_xml
     end
 
   end
