4ec6e11a0e80a45395e3d0f2be400576f6d223af
Peter Mangiafico <peter@mangiafico.org>
2012-05-01 15:15:58 -0700

    assumed flattened folder structure for CM for SMPL projects

--- spec/project_specific_spec.rb
+++ spec/project_specific_spec.rb
@@ -33,37 +33,37 @@ describe PreAssembly::DigitalObject do
       <contentMetadata objectId="aa111aa1111" type="file">
         <resource sequence="1" id="aa111aa1111_1" type="file">
           <label>Tape 1, Side A</label>
-          <file shelve="no" publish="yes" preserve="yes" id="PM/aa111aa1111_001_a_pm.wav">
+          <file shelve="no" publish="yes" preserve="yes" id="aa111aa1111_001_a_pm.wav">
             <provider_checksum type="md5">checksumforaa111aa1111_001_a_pm.wav</provider_checksum>
           </file>
-          <file shelve="no" publish="yes" preserve="yes" id="SH/aa111aa1111_001_a_sh.wav">
+          <file shelve="no" publish="yes" preserve="yes" id="aa111aa1111_001_a_sh.wav">
             <provider_checksum type="md5">checksumforaa111aa1111_001_a_sh.wav</provider_checksum>
           </file>
-          <file shelve="no" publish="yes" preserve="yes" id="SL/aa111aa1111_001_a_sl.mp3">
+          <file shelve="no" publish="yes" preserve="yes" id="aa111aa1111_001_a_sl.mp3">
             <provider_checksum type="md5">checksumforaa111aa1111_001_a_sl.mp3</provider_checksum>
           </file>
-          <file shelve="yes" publish="yes" preserve="yes" id="Images/aa111aa1111_001_img_1.jpg">
+          <file shelve="yes" publish="yes" preserve="yes" id="aa111aa1111_001_img_1.jpg">
             <provider_checksum type="md5">checksumforaa111aa1111_001_img_1.jpg</provider_checksum>
           </file>
         </resource>
         <resource sequence="2" id="aa111aa1111_2" type="file">
           <label>Tape 1, Side B</label>
-          <file shelve="no" publish="yes" preserve="yes" id="PM/aa111aa1111_001_b_pm.wav">
+          <file shelve="no" publish="yes" preserve="yes" id="aa111aa1111_001_b_pm.wav">
             <provider_checksum type="md5">checksumforaa111aa1111_001_b_pm.wav</provider_checksum>
           </file>
-          <file shelve="no" publish="yes" preserve="yes" id="SH/aa111aa1111_001_b_sh.wav">
+          <file shelve="no" publish="yes" preserve="yes" id="aa111aa1111_001_b_sh.wav">
             <provider_checksum type="md5">checksumforaa111aa1111_001_b_sh.wav</provider_checksum>
           </file>
-          <file shelve="no" publish="yes" preserve="yes" id="SL/aa111aa1111_001_b_sl.mp3">
+          <file shelve="no" publish="yes" preserve="yes" id="aa111aa1111_001_b_sl.mp3">
             <provider_checksum type="md5">checksumforaa111aa1111_001_b_sl.mp3</provider_checksum>
           </file>
-          <file shelve="yes" publish="yes" preserve="yes" id="Images/aa111aa1111_001_img_2.jpg">
+          <file shelve="yes" publish="yes" preserve="yes" id="aa111aa1111_001_img_2.jpg">
             <provider_checksum type="md5">checksumforaa111aa1111_001_img_2.jpg</provider_checksum>
           </file>
         </resource>
         <resource sequence="3" id="aa111aa1111_3" type="file">
           <label>Transcript</label>
-          <file shelve="yes" publish="yes" preserve="yes" id="Transcript/aa111aa1111.pdf">
+          <file shelve="yes" publish="yes" preserve="yes" id="aa111aa1111.pdf">
             <provider_checksum type="md5">checksumforedited_transcript.pdf</provider_checksum>
           </file>
         </resource>
