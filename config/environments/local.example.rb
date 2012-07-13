495cb9a013d090559241a74954132dd5625c68d8
Peter Mangiafico <peter@mangiafico.org>
2012-07-13 12:06:08 -0700

    allow the web service call to retry 5 times, sleeping between each call in order to avoid pre-assembly crashing if the dor web service is temporarily unavailable

---
+++ config/environments/local.example.rb
@@ -0,0 +1,38 @@
+cert_dir = File.join(File.dirname(__FILE__), "..", "certs")
+
+Dor::Config.configure do
+
+  fedora do
+    url 'http://USERNAME:PASSWORD@dor-dev.stanford.edu/fedora'
+  end
+  
+  ssl do
+    cert_file File.join(cert_dir,"dlss-dev-test.crt")
+    key_file  File.join(cert_dir,"dlss-dev-test.key")
+    key_pass ''
+  end
+  
+  suri do
+    mint_ids true
+    id_namespace 'druid'
+    url 'http://lyberservices-dev.stanford.edu'
+    user 'labware'
+    pass 'lyberteam'
+  end
+
+  metadata do
+    exist.url 'http://viewer:l3l%40nd@lyberapps-prod.stanford.edu/exist/rest/'
+    catalog.url 'http://lyberservices-prod.stanford.edu/catalog/mods'
+  end
+ 
+  gsearch.url  'https://dor-dev.stanford.edu/solr/gsearch'
+  solrizer.url 'https://dor-dev.stanford.edu/solr/solrizer'
+  workflow.url 'http://lyberservices-dev.stanford.edu/workflow/'
+
+  dor do
+    service_root 'http://USERNAME:PASSWORD@lyberservices-dev.stanford.edu'
+    num_attempts  5  # the number of attempts to contact the dor web service before throwing an exception
+    sleep_time    10  # sleep time in seconds between attempts to contact the dor service    
+  end
+  
+end
