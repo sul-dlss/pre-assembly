51b75ac6c61c1b3c31e266f3ddeba01e49fb6e0b
Peter Mangiafico <peter@mangiafico.org>
2012-10-02 16:15:05 -0700

    updates needed to get capistrano deployment working

--- config/boot.rb
+++ config/boot.rb
@@ -1,8 +1,10 @@
 require 'rubygems'
+require 'bundler/setup'
 require 'logger'
 
 environment  = ENV['ROBOT_ENVIRONMENT'] ||= 'development'
 PRE_ASSEMBLY_ROOT = File.expand_path(File.dirname(__FILE__) + '/..')
+CERT_DIR = File.join(File.dirname(__FILE__), ".", "certs")
 
 # these are the names of special datastream files that will be staged in the 'metadata' folder instead of the 'content' folder
 METADATA_FILES=['descMetadata.xml'].map(&:downcase)
