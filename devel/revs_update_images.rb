5c6f5bf3de0ac5ad9ac1a69f0cd3a8094da6bc60
Peter Mangiafico <peter@mangiafico.org>
2013-06-17 17:04:07 -0700

    bug fixes to image remediation script

--- devel/revs_update_images.rb
+++ devel/revs_update_images.rb
@@ -1,7 +1,7 @@
 # Used to update images from an update set of image files and a small manifest containing sourceIDs
 # Iterate through each image in the manifest, use source ID to lookup DRUID, add the new image to the object, re-shelve it, and then
 # update contentMetadata using the remediation framework.  Since we are not re-generating descMetadata, the only required columns in
-# the manifest are 'sourceid' and 'filename'
+# the manifest are 'sourceid' and 'filename' (include the header row with these columns)
 
 # Peter Mangiafico
 # June 17, 2013
@@ -20,6 +20,7 @@ require 'csv-mapper'
 include CsvMapper
 
 remediate_logic_file='/thumpers/dpgthumper-staging/Revs/revs_update_images_remediation.rb' # the revs update mods remeditation class
+#remediate_logic_file='/dor/preassembly/revs/revs_update_images_remediation.rb' # the revs update mods remeditation class
 require remediate_logic_file
 
 source_path=File.dirname(csv_in)
@@ -34,20 +35,24 @@ completed_druids=PreAssembly::Remediation::Item.get_druids(progress_log_file)
 @items=CsvMapper.import(csv_in) do read_attributes_from_file end
   
 @items.each_with_index do |row, x|
-  pid=Dor.find_by_sourceid("Revs:#{manifest_row[:sourceid]}") # grab the PID given the sourceID
-#  pid='druid:qh202yd6550'  
-  done=completed_druids.include?(pid)
-   if done 
-     puts "#{pid} : skipping, already completed"
-   else
-     data={:new_image=>File.join(source_path,row.filename),:environment=>ENV['ROBOT_ENVIRONMENT']}
-     item=PreAssembly::Remediation::Item.new(pid,data)
-     item.description="Updating image from #{csv_in}" # added to the version description
-     item.extend(RemediationLogic) # add in our project specific methods
-     success=item.remediate
-     item.log_to_progress_file(progress_log_file)
-     item.log_to_csv(csv_out)
-     puts "#{pid} : #{success}"    
+  pids=Dor::SearchService.query_by_id("Revs:#{row.sourceid}")
+  if pids.size != 1
+    puts "cannot find single pid for source id #{row.sourceid}"
+  else
+    pid=pids.first
+    done=completed_druids.include?(pid)
+     if done 
+       puts "#{pid} : skipping, already completed"
+     else
+       data={:source_path=>source_path,:filename=>row.filename,:environment=>ENV['ROBOT_ENVIRONMENT']}
+       item=PreAssembly::Remediation::Item.new(pid,data)
+       item.description="Updating image from #{csv_in}" # added to the version description
+       item.extend(RemediationLogic) # add in our project specific methods
+       success=item.remediate
+       item.log_to_progress_file(progress_log_file)
+       item.log_to_csv(csv_out)
+       puts "#{pid} : #{success}"    
+     end
    end
    
 end
