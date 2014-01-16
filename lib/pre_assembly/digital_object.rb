c990a28316df3e137400fcdf66efbc80b9c691cb
Peter Mangiafico <peter@mangiafico.org>
2014-01-16 13:41:42 -0800

    restore delete from solr when object reg fails

--- lib/pre_assembly/digital_object.rb
+++ lib/pre_assembly/digital_object.rb
@@ -272,12 +272,12 @@ module PreAssembly
           all_pids.each do |pid|
             begin
               Dor::Config.fedora.client["objects/#{pid}"].delete
-             # Dor::SearchService.solr.delete_by_id(pid)  # should be unncessary
+              Dor::SearchService.solr.delete_by_id(pid)  # should be unncessary
             rescue Exception => e
               log "      ... could not delete object with #{pid} or source id #{source_id} : #{e.message} ..."
             end
           end
-          #Dor::SearchService.solr.commit
+          Dor::SearchService.solr.commit
           backtrace=e.backtrace
           exception_message=e.message
           sleep Dor::Config.dor.sleep_time
