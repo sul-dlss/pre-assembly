67a3c236d609551dd50a115f6dc7b9ae9f661d55
Peter Mangiafico <peter@mangiafico.org>
2014-03-07 10:48:31 -0800

    add a devel script to batch reset workflow steps

--- devel/reset_workflow_step.rb
+++ devel/reset_workflow_step.rb
@@ -13,12 +13,15 @@ require 'logger'
 require 'net/ssh'
 
 dry_run=false # if set to true, then no operations are actually carried out, you only get notices of what will happen
-log_file='/dor/preassembly/revs/revs_ludvigsen-1972_log.yaml'
+log_file='/dor/preassembly/revs/revs_ludvigsen-1971_log.yaml'
+workflow='accessionWF'
+step='rights-metadata'
+state='waiting'
 
 completed_druids=Assembly::Utils.get_druids_from_log(log_file,true)
 puts "found #{completed_druids.size} druids"
 completed_druids.each_with_index do |druid,index|
   desc_filename=File.join(DruidTools::Druid.new(druid,'/dor/assembly').path(),'metadata','descMetadata.xml')
-  puts "#{druid} [#{index+1} of #{completed_druids.size}]: touching #{desc_filename}"
-  `touch #{desc_filename}` unless dry_run 
+  puts "#{druid} [#{index+1} of #{completed_druids.size}]: reseting #{workflow}:#{step} to #{state}"
+  Dor::WorkflowService.update_workflow_status('dor',druid,workflow, step, state) unless dry_run
 end
