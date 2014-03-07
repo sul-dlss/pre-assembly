bec75be971791cb83f4672abe8b1776b8f0bdeee
Peter Mangiafico <peter@mangiafico.org>
2014-03-06 16:36:19 -0800

    add a script for bulk touching of descmetadata.xml to advance timestamps

---
+++ devel/touch_descmetadata.rb
@@ -0,0 +1,24 @@
+# run with ruby devel/add_and_update_files.rb
+# must be run from lyberservices-prod to have access to all mounts and configuration
+
+#!/usr/bin/env ruby
+ENV['ROBOT_ENVIRONMENT']='production'  # environment to run under (i.e. which fedora instance to hit)
+
+require File.expand_path(File.dirname(__FILE__) + '/../config/boot')
+
+require 'rubygems'
+require 'dor-services'
+require 'assembly-utils'
+require 'logger'
+require 'net/ssh'
+
+dry_run=false # if set to true, then no operations are actually carried out, you only get notices of what will happen
+log_file='/dor/preassembly/revs/revs_ludvigsen-1972_log.yaml'
+
+completed_druids=Assembly::Utils.get_druids_from_log(log_file,true)
+puts "found #{completed_druids.size} druids"
+completed_druids.each do |druid|
+  desc_filename=File.join(DruidTools::Druid.new(druid,'/dor/assembly').path(),'metadata','descMetadata.xml')
+  puts "#{druid}: touching #{desc_filename}"
+  `touch #{desc_filename}` unless dry_run 
+end
