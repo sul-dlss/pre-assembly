ae78bbfc3fb16386c6cccea050e084f473d20054
Chris Beer <cabeer@stanford.edu>
2014-08-01 11:38:03 -0700

    bundle without the 'test' group in -dev

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -2,4 +2,4 @@ server 'sul-lyberservices-dev.stanford.edu', user: 'lyberadmin', roles: %w{web a
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, "development"
-set :bundle_without, ""
+set :bundle_without, "test"
