798187aa9b0a524f798316fea0f79b640eba5162
Carrick Rogers <carrickr@stanford.edu>
2014-11-26 14:20:28 -0800

    Debug.

--- bin/get_dor_and_sdr_versions.rb
+++ bin/get_dor_and_sdr_versions.rb
@@ -75,7 +75,10 @@ druids.each do |druid| #TODO: Threach me
     
     #Get SDR Version
     begin
+      puts "#{ARGV[2]}/sdr/objects/#{druid}/current_version"
+      puts "#{ARGV[3]},#{ARGV[4]}"
       r = Nokogiri::HTML(open("#{ARGV[2]}/sdr/objects/#{druid}/current_version",:http_basic_authentication=>["#{ARGV[3]},#{ARGV[4]}"]))
+      puts r
       sdr_version = r.xpath('//currentversion').children.first.text.to_i
     rescue 
       @run_log.error("The object #{druid} failed to a return sdr_version")
