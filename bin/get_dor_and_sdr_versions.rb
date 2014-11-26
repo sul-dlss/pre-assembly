a808c8e079cecf1dabf85f7e92c7ca728c2e3f46
Carrick Rogers <carrickr@stanford.edu>
2014-11-26 14:11:25 -0800

    Fix sdr xpath

--- bin/get_dor_and_sdr_versions.rb
+++ bin/get_dor_and_sdr_versions.rb
@@ -75,13 +75,8 @@ druids.each do |druid| #TODO: Threach me
     
     #Get SDR Version
     begin
-      puts "#{ARGV[2]}/sdr/objects/#{druid}/current_version,:http_basic_authentication=>[#{ARGV[3]},#{ARGV[4]}]"
-      puts "Result of call:" 
-      puts open("#{ARGV[2]}/sdr/objects/#{druid}/current_version,:http_basic_authentication=>[#{ARGV[3]},#{ARGV[4]}]")
       r = Nokogiri::HTML(open("#{ARGV[2]}/sdr/objects/#{druid}/current_version",:http_basic_authentication=>["#{ARGV[3]},#{ARGV[4]}]"))
-      puts r
-      @run_log.info(r)
-      #sdr_version = r.xpath('//currentversion').children.first.text.to_i
+      sdr_version = r.xpath('//currentversion').children.first.text.to_i
     rescue 
       @run_log.error("The object #{druid} failed to a return sdr_version")
       next #Skip to the next druid
