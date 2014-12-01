1dfcdd7662870d905128f21238dd89bdfe5fd703
Carrick Rogers <carrickr@stanford.edu>
2014-12-01 14:31:36 -0800

    Current path fix.

--- bin/get_status_for_versions.rb
+++ bin/get_status_for_versions.rb
@@ -6,6 +6,7 @@ require 'rubygems'
 require 'dor-services'
 require 'csv'
 
+current_path = File.dirname(File.expand_path(__FILE__))
 output_path = current_path.split("/bin")[0] + "/log/add_status/"
 @with_status = CSV.open(output_path+"results.csv",'wb')
 
