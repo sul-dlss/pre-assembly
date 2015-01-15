508e8dfb575db2c492320ba8ee059f4ff85c2f9d
Peter Mangiafico <peter@mangiafico.org>
2015-01-14 16:23:23 -0800

    update to latest version of revs-utils and add a new revs specific MODs template test

--- spec/revs_project_specific_spec.rb
+++ spec/revs_project_specific_spec.rb
@@ -12,142 +12,144 @@ describe PreAssembly::DigitalObject do
     @tmp_dir_args = [nil, 'tmp']
     @dobj.object_files = []
     @dobj.desc_md_template_xml = <<-END.gsub(/^ {8}/, '')
-      <?xml version="1.0"?>
-      <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.loc.gov/mods/v3" version="3.3" xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-3.xsd">
-        <typeOfResource>still image</typeOfResource>
-        <genre authority="aat">digital image</genre>
-        <subject displayLabel="Subject" authority="lcsh">
-          <topic>Automobile</topic>
-          <topic>History</topic>
-        </subject>
-      	<% if !manifest_row[:marque].blank? %>
-       		<% manifest_row[:marque].split(/[,|]/).each do |marque| %>
-       			<% lc_term=revs_lookup_marque(marque.strip)
-       			 if lc_term %>
-       			    <subject displayLabel="Marque" authority="lcsh" authorityURI="http://id.loc.gov/authorities/subjects">
-                    <topic valueURI="<%=lc_term['url']%>"><%=marque.strip%></topic>
-                </subject>
-      		   <% else %>
-          			<subject displayLabel="Marque" authority="local">
-          				<topic><%=marque.strip%></topic>
-          			</subject>         			   
-      		   <% end %>
-       		<% end %>
-         <% end %>
-      	<% if !manifest_row[:model].blank? %>
-       		<% manifest_row[:model].split(/[,|]/).each do |model| %>
-      		<subject displayLabel="Model" authority="local">
-      			<topic><%=model.strip%></topic>
-      		</subject>
-      		<% end %>
-      	<% end %>
-      	<% if !manifest_row[:people].blank? %>
-      		<% manifest_row[:people].split('|').each do |person| %>
-      			<subject displayLabel="People" authority="local">
-      				<name type="personal"><namePart><%=person.strip%></namePart></name>
-      			</subject>
-      		<% end %>
-        <% end %>
-        <% if !manifest_row[:entrant].blank? %>
-          <% manifest_row[:entrant].split('|').each do |entrant| %>
-            <subject id="entrant" displayLabel="Entrant" authority="local">
-              <name type="personal"><namePart><%=entrant.strip%></namePart></name>
-            </subject>
-          <% end %>
-        <% end %>
-      	<% if !manifest_row[:photographer].blank? %>
-      		<name id="photographer" displayLabel="Photographer" type="personal" authority="local">
-      	    <namePart><%=manifest_row[:photographer].strip%></namePart>
-      	    <role><roleTerm type="text" authorityURI="http://id.loc.gov/vocabulary/relators/pht">Photographer</roleTerm></role>
-      		</name>
-        <% end %>
-      	<% if !manifest_row[:current_owner].blank? %>
-      		<subject id="current_owner" displayLabel="Current Owner" authority="local">
-      			<name type="personal"><namePart><%=manifest_row[:current_owner].strip%></namePart></name>
-      		</subject>
-        <% end %>
-        <% if !manifest_row[:venue].blank? %>
-      		<subject id="venue" displayLabel="Venue" authority="local">
-      			<topic><%=manifest_row[:venue].strip%></topic>
+    <?xml version="1.0"?>
+    <mods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.loc.gov/mods/v3" version="3.3" xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-3.xsd">
+      <typeOfResource>still image</typeOfResource>
+      <genre authority="aat">digital image</genre>
+      <subject displayLabel="Subject" authority="lcsh">
+        <topic>Automobile</topic>
+        <topic>History</topic>
+      </subject>
+    	<% if !manifest_row[:marque].blank? %>
+     		<% manifest_row[:marque].split(/[,|]/).each do |marque| %>
+     			<% lc_term=revs_lookup_marque(marque.strip)
+     			 if lc_term %>
+     			    <subject displayLabel="Marque" authority="lcsh" authorityURI="http://id.loc.gov/authorities/subjects">
+                <topic valueURI="<%=lc_term['url']%>"><%=marque.strip%></topic>
+              </subject>
+    		   <% else %>
+        			<subject displayLabel="Marque" authority="local">
+        				<topic><%=marque.strip%></topic>
+        			</subject>         			   
+    		   <% end %>
+     		<% end %>
+       <% end %>
+    	<% if !manifest_row[:model].blank? %>
+     		<% manifest_row[:model].split(/[,|]/).each do |model| %>
+    		<subject displayLabel="Model" authority="local">
+    			<topic><%=model.strip%></topic>
+    		</subject>
+    		<% end %>
+    	<% end %>
+    	<% if !manifest_row[:people].blank? %>
+    		<% manifest_row[:people].split('|').each do |person| %>
+    			<subject displayLabel="People" authority="local">
+    				<name type="personal"><namePart><%=person.strip%></namePart></name>
+    			</subject>
+    		<% end %>
+      <% end %>
+    	<% if !manifest_row[:entrant].blank? %>
+    	  <% manifest_row[:entrant].split('|').each do |entrant| %>
+      		<subject id="entrant" displayLabel="Entrant" authority="local">
+      			<name type="personal"><namePart><%=entrant.strip%></namePart></name>
       		</subject>
       	<% end %>
-        <% if !manifest_row[:track].blank? %>
-      		<subject id="track" displayLabel="Track" authority="local">
-      			<topic><%=manifest_row[:track].strip%></topic>
-      		</subject>
-      	<% end %>
-        <% if !manifest_row[:event].blank? %>
-      		<subject id="event" displayLabel="Event" authority="local">
-      			<topic><%=manifest_row[:event].strip%></topic>
-      		</subject>
-      	<% end %>
-      	<% if !manifest_row[:country].blank? || !manifest_row[:city].blank? || !manifest_row[:state].blank?  %>
-       		<subject id="location" displayLabel="Location" authority="local">
-      	    <hierarchicalGeographic>
-      		     <%if !manifest_row[:country].blank? %>
-      			     	<country><%=manifest_row[:country].strip%></country>
-      			 <% end %>
-          	   <% if !manifest_row[:state].blank? %>
-      	      	   <state><%=manifest_row[:state].strip%></state>
-          	   	 <% end %>  
-          	   <% if !manifest_row[:city].blank? %>
-          	   	         <city><%=manifest_row[:city].strip%></city>
-          	   	 <% end %>  	   	 
-      			</hierarchicalGeographic>
-      		</subject>
-        <% elsif !manifest_row[:location].blank? %>
-       		<subject id="location" displayLabel="Location" authority="local">
-      	    <hierarchicalGeographic>
-      		<% manifest_row[:location].split(/[,|]/).reverse.each do |location| %>
-      		  <% country=revs_get_country(location) 
-      				 city_state=revs_get_city_state(location)
-      		     if country %>
-      			     	<country><%=country.strip%></country>
-          	   <% elsif city_state %>
-      	         <state><%=revs_get_state_name(city_state[1].strip)%></state>
-      	         <city><%=city_state[0].strip%></city>
-      				<% else %>
-      					<citySection><%=location.strip%></citySection>
-      				<% end %>
-      		<% end %>
-      			</hierarchicalGeographic>
-      		</subject>
-      	<% end %>
-      	<relatedItem type="host">
-          <titleInfo>
-            <title>The Collier Collection of the Revs Institute for Automotive Research</title>
-          </titleInfo>
-          <typeOfResource collection="yes"/>
-        </relatedItem>
+      <% end %>
+    	<% if !manifest_row[:photographer].blank? %>
+    		<name id="photographer" displayLabel="Photographer" type="personal" authority="local">
+    	    <namePart><%=manifest_row[:photographer].strip%></namePart>
+    	    <role><roleTerm type="text" authorityURI="http://id.loc.gov/vocabulary/relators/pht">Photographer</roleTerm></role>
+    		</name>
+      <% end %>
+    	<% if !manifest_row[:current_owner].blank? %>
+    		<subject id="current_owner" displayLabel="Current Owner" authority="local">
+    			<name type="personal"><namePart><%=manifest_row[:current_owner].strip%></namePart></name>
+    		</subject>
+      <% end %>
+      <% if !manifest_row[:venue].blank? %>
+    		<subject id="venue" displayLabel="Venue" authority="local">
+    			<topic><%=manifest_row[:venue].strip%></topic>
+    		</subject>
+    	<% end %>
+      <% if !manifest_row[:track].blank? %>
+    		<subject id="track" displayLabel="Track" authority="local">
+    			<topic><%=manifest_row[:track].strip%></topic>
+    		</subject>
+    	<% end %>
+      <% if !manifest_row[:event].blank? %>
+    		<subject id="event" displayLabel="Event" authority="local">
+    			<topic><%=manifest_row[:event].strip%></topic>
+    		</subject>
+    	<% end %>
+    	<% if !manifest_row[:country].blank? || !manifest_row[:city].blank? || !manifest_row[:state].blank?  %>
+     		<subject id="location" displayLabel="Location" authority="local">
+    	    <hierarchicalGeographic>
+    		     <%if !manifest_row[:country].blank? %>
+    			     	<country><%=manifest_row[:country].strip%></country>
+    			 <% end %>
+        	   <% if !manifest_row[:state].blank? %>
+    	      	   <state><%=manifest_row[:state].strip%></state>
+        	   	 <% end %>  
+        	   <% if !manifest_row[:city].blank? %>
+        	   	         <city><%=manifest_row[:city].strip%></city>
+        	   	 <% end %>  	   	 
+    			</hierarchicalGeographic>
+    		</subject>
+      <% elsif !manifest_row[:location].blank? %>
+     		<subject id="location" displayLabel="Location" authority="local">
+    	    <hierarchicalGeographic>
+    		<% manifest_row[:location].split(/[,|]/).reverse.each do |location| %>
+    		  <% country=revs_get_country(location) 
+    				 city_state=revs_get_city_state(location)
+    		     if country %>
+    			     	<country><%=country.strip%></country>
+        	   <% elsif city_state %>
+    	         <state><%=revs_get_state_name(city_state[1].strip)%></state>
+    	         <city><%=city_state[0].strip%></city>
+    				<% else %>
+    					<citySection><%=location.strip%></citySection>
+    				<% end %>
+    		<% end %>
+    			</hierarchicalGeographic>
+    		</subject>
+    	<% end %>
+    	<relatedItem type="host">
+        <titleInfo>
+          <title>The Collier Collection of the Revs Institute for Automotive Research</title>
+        </titleInfo>
+        <typeOfResource collection="yes"/>
+      </relatedItem>
+    	<% manifest_row[:format].split('|').each do |format| %>  
         <relatedItem type="original">
           <physicalDescription>
-            <form authority="aat"><%=revs_check_format(manifest_row[:format]).strip%></form>
+            <form authority="aat"><%=revs_check_format(format.downcase.strip)%></form>
           </physicalDescription>
         </relatedItem>
-        <% if !manifest_row[:date].blank? %>
+      <% end %>
+      <% if !manifest_row[:date].blank? %>
+        <originInfo keyDate="yes">
+          <dateCreated><%=manifest_row[:date].strip%></dateCreated>
+        </originInfo>
+      <% elsif !manifest_row[:year].blank? %>	
           <originInfo keyDate="yes">
-            <dateCreated><%=manifest_row[:date].strip%></dateCreated>
+            <dateCreated><%=manifest_row[:year].strip%></dateCreated>
           </originInfo>
-        <% elsif !manifest_row[:year].blank? %>	
-            <originInfo keyDate="yes">
-              <dateCreated><%=manifest_row[:year].strip%></dateCreated>
-            </originInfo>
-        <% end %>
-        <titleInfo>
-          <title><% if !manifest_row[:label].blank? %><%=manifest_row[:label].strip%><% end %></title>
-        </titleInfo>
-        <identifier type="local" displayLabel="Revs ID">[[sourceid]]</identifier>
-        <% if !manifest_row[:description].blank? %><note displayLabel="Description"><%=manifest_row[:description].strip%></note><% end %>  
-        <% if !manifest_row[:model_year].blank? %><note displayLabel="Model Year" ID="model_year"><%=manifest_row[:model_year].strip%></note><% end %>
-        <% if !manifest_row[:group_or_class].blank? %><note displayLabel="Group or Class" ID="group"><%=manifest_row[:group_or_class].strip%></note><% end %>
-        <% if !manifest_row[:race_data].blank? %><note displayLabel="Race Data" ID="race_data"><%=manifest_row[:race_data].strip%></note><% end %>
-        <% if !manifest_row[:metadata_sources].blank? %><note displayLabel="Metadata Sources" ID="metadata_sources"><%=manifest_row[:metadata_sources].strip%></note><% end %>
-        <% if !manifest_row[:vehicle_markings].blank? %><note displayLabel="Vehicle Markings" ID="vehicle_markings"><%=manifest_row[:vehicle_markings].strip%></note><% end %>
-        <% if !manifest_row[:inst_notes].blank? %><note type="source note" displayLabel="Institution Notes" ID="inst_notes"><%=manifest_row[:inst_notes].strip%></note><% end %>
-        <% if !manifest_row[:prod_notes].blank? %><note type="source note" displayLabel="Production Notes" ID="prod_notes"><%=manifest_row[:prod_notes].strip%></note><% end %>
-        <% if !manifest_row[:has_more_metadata].blank? %><note type="source note" displayLabel="Has More Metadata" ID="has_more_metadata">yes</note><% end %>
-        <% if !manifest_row[:hide].blank? %><note type="source note" displayLabel="Visibility" ID="visibility">hidden</note><% end %>
-      </mods>
+      <% end %>
+      <titleInfo>
+        <title><% if !manifest_row[:label].blank? %><%=manifest_row[:label].strip%><% end %></title>
+      </titleInfo>
+      <identifier type="local" displayLabel="Revs ID">[[sourceid]]</identifier>
+      <% if !manifest_row[:description].blank? %><note displayLabel="Description"><%=manifest_row[:description].strip%></note><% end %>  
+      <% if !manifest_row[:model_year].blank? %><note displayLabel="Model Year" ID="model_year"><%=manifest_row[:model_year].strip%></note><% end %>
+      <% if !manifest_row[:group_or_class].blank? %><note displayLabel="Group or Class" ID="group"><%=manifest_row[:group_or_class].strip%></note><% end %>
+      <% if !manifest_row[:race_data].blank? %><note displayLabel="Race Data" ID="race_data"><%=manifest_row[:race_data].strip%></note><% end %>
+      <% if !manifest_row[:metadata_sources].blank? %><note displayLabel="Metadata Sources" ID="metadata_sources"><%=manifest_row[:metadata_sources].strip%></note><% end %>
+      <% if !manifest_row[:vehicle_markings].blank? %><note displayLabel="Vehicle Markings" ID="vehicle_markings"><%=manifest_row[:vehicle_markings].strip%></note><% end %>
+      <% if !manifest_row[:inst_notes].blank? %><note type="source note" displayLabel="Institution Notes" ID="inst_notes"><%=manifest_row[:inst_notes].strip%></note><% end %>
+      <% if !manifest_row[:prod_notes].blank? %><note type="source note" displayLabel="Production Notes" ID="prod_notes"><%=manifest_row[:prod_notes].strip%></note><% end %>
+      <% if !manifest_row[:has_more_metadata].blank? %><note type="source note" displayLabel="Has More Metadata" ID="has_more_metadata">yes</note><% end %>
+      <% if !manifest_row[:hide].blank? %><note type="source note" displayLabel="Visibility" ID="visibility">hidden</note><% end %>
+    </mods>
     END
     
   end
@@ -294,7 +296,7 @@ describe PreAssembly::DigitalObject do
 
    end 
 
-   describe "revs specific descriptive metadata using other lookup methods for location tags with no known entities" do
+   describe "revs specific descriptive metadata using other lookup methods for location tags with no known entities and multiple formats with format correction" do
 
      before(:each) do
        @dobj.druid = @druid
@@ -302,9 +304,9 @@ describe PreAssembly::DigitalObject do
          :sourceid    => 'foo-1',
          :label       => 'a label',
          :date        => '9/2/2012',
+         :format      => 'black-and-white negative| color transparencies',
          :year        => 'blot',
          :description => 'this is a description > another description < other stuff',
-         :format      => 'black-and-white negative',
          :location    => 'Raceway | Random City | Random Country'
        }
        @exp_xml = <<-END.gsub(/^ {8}/, '')
@@ -334,6 +336,11 @@ describe PreAssembly::DigitalObject do
              <form authority="aat">black-and-white negatives</form>
            </physicalDescription>
          </relatedItem>
+         <relatedItem type="original">
+           <physicalDescription>
+             <form authority="aat">color transparencies</form>
+           </physicalDescription>
+         </relatedItem>         
          <originInfo keyDate="yes">
            <dateCreated>9/2/2012</dateCreated>
          </originInfo>
