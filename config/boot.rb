3c3131a1590227b5dc73dca86a1cdb1868862029
Chris Beer <cabeer@stanford.edu>
2018-02-21 08:37:16 -0800

    As of active-fedora 8.x, the mediashelf-loggable gem that injected logger methods everywhere is gone

--- config/boot.rb
+++ config/boot.rb
@@ -6,18 +6,8 @@ environment  = ENV['ROBOT_ENVIRONMENT'] ||= 'development'
 PRE_ASSEMBLY_ROOT = File.expand_path(File.dirname(__FILE__) + '/..')
 CERT_DIR = File.join(File.dirname(__FILE__), ".", "certs")
 
-# Override Solrizer logger before it gets a chance to load and pollute STDERR.
-require 'solrizer'
-if Solrizer.respond_to? :logger
-  solr_log        = Logger.new(PRE_ASSEMBLY_ROOT + "/log/solrizer_#{environment}.log")
-  solr_log.level  = Logger::SEV_LABEL.index(ENV['ROBOT_LOG_LEVEL']) || Logger::INFO
-  Solrizer.logger = solr_log
-end
-
 # General DLSS infrastructure.
 require 'dor-services'
-
-ActiveFedora::Base.logger.level = Logger::ERROR # make activefedora logging much less noisy
 
 # Environment.
 unless defined?(NO_ENVIRONMENT)
