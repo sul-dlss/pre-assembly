6c9caeb199b25915722f39d2f750013ae9f0a27f
Tony Zanella <tony.zanella@gmail.com>
2018-06-29 12:19:56 -0700

    Deploy pre-assembly to new machines

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,4 +1,9 @@
-server 'sul-lyberservices-prod.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
+server 'sul-preassembly-prod.stanford.edu', user: 'preassembly', roles: %w{web app db}
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, "production"
+
+set :rvm_ruby_version, '2.4.4'
+set :deploy_to, '/opt/app/preassembly/pre-assembly'
+set :linked_files, []
+set :repo_url, 'ssh://git@github.com/sul-dlss/pre-assembly'
