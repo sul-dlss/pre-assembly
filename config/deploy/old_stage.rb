1a342e9d8a0e710bfefa214d991b7eb82dc24f19
Joe Atzberger <ohiocore@gmail.com>
2018-08-21 17:24:39 -0700

    Baseline deploy.rb should suit new systems, not lyberservices
    Effectively, this treats the "old" sets as the exception rather than the
    rule.  Note: deploying master to lyberservices will break things.  Use
    the `v3-legacy` branch for that.
    
    Remove ignored/redundant rvm lines (we have `.ruby-version` file).
    
    Use ssh to access github.

--- config/deploy/old_stage.rb
+++ config/deploy/old_stage.rb
@@ -2,3 +2,4 @@ server 'sul-lyberservices-test.stanford.edu', user: 'lyberadmin', roles: %w{web 
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, "test"
+set :deploy_to, '/home/lyberadmin/pre-assembly'
