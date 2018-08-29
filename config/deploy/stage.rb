d4e722b78f5124aa243cdd4baafc2230a75f0c6b
Peter Mangiafico <peter@mangiafico.org>
2018-08-29 12:05:08 -0700

    set deployment targets back to lyberservices-XX by default

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,9 +1,4 @@
-server 'sul-preassembly-stage.stanford.edu', user: 'preassembly', roles: %w{web app db}
+server 'sul-lyberservices-test.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
 
 Capistrano::OneTimeKey.generate_one_time_key!
-set :rails_env, "production"
-
-set :rvm_ruby_version, '2.4.4'
-set :deploy_to, '/opt/app/preassembly/pre-assembly'
-set :linked_files, []
-set :repo_url, 'ssh://git@github.com/sul-dlss/pre-assembly'
+set :rails_env, "test"
