4ec55b45db3ecf50ea1f5de1200bf7c715868689
Naomi Dushay <ndushay@stanford.edu>
2018-08-29 18:45:26 -0700

    deploy.rb: more info about honeybadger notification problem

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,11 +42,22 @@ set :linked_dirs, %w{log config/certs config/cli_environments tmp vendor/bundle}
 
 set :honeybadger_env, fetch(:stage)
 
-
 Rake::Task["honeybadger:deploy"].clear
 namespace :honeybadger do
   desc 'Notify Honeybadger of the deployment.'
   task :deploy => [:'deploy:set_current_revision'] do
-    puts "Temoporarily disabling honeybadger deploy notification, until we fix it."
+    puts "Temporarily disabling honeybadger deploy notification due to bug with Haml::ActionView. See issue https://github.com/sul-dlss/pre-assembly/issues/98"
+    #  from capistrano messages, we get:
+    #  "NameError: uninitialized constant Haml::ActionView
+    # 01     /opt/app/preassembly/pre-assembly/shared/bundle/ruby/2.4.0/gems/haml-4.0.7/lib/haml/helpers/safe_erubis_template.rb:3:in `<module:Haml>"
+    #
+    # see https://github.com/haml/haml/issues/974
+    #
+    # from bundler, can't update to haml 5.0.4:
+    # dor-services (~> 5.29) was resolved to 5.29.0, which depends on
+    #   active-fedora (< 9.a, >= 6.0) was resolved to 8.5.0, which depends on
+    #     rdf-rdfxml (~> 1.1) was resolved to 1.99.0, which depends on
+    #       rdf-rdfa (~> 1.99) was resolved to 1.99.1, which depends on
+    #         haml (~> 4.0)
   end
 end
