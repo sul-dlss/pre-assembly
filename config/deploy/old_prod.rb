02c4675cef732954aae7359bf0381f2772f07ea1
Peter Mangiafico <peter@mangiafico.org>
2018-08-30 11:05:01 -0700

    setup old_prod and old_stage to deploy correct branch to lyberservices-XX environments

--- config/deploy/old_prod.rb
+++ config/deploy/old_prod.rb
@@ -1,5 +1,8 @@
 server 'sul-lyberservices-prod.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
+set :repo_url, 'https://github.com/sul-dlss/pre-assembly.git'
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, "production"
-set :deploy_to, '/home/lyberadmin/pre-assembly'
+set :branch, 'v3-legacy'
+
+set :honeybadger_env, 'lyberservices-prod'
