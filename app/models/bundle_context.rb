ba7cf3c2c09946fa1786f2c47aae5e7ae663dbfc
Sarav Shah <saravsshah@gmail.com>
2018-09-05 15:51:38 -0700

    Add has_many and belongs_to associations to User Model, and add enums for two of the attributes in bundlecontext

--- app/models/bundle_context.rb
+++ app/models/bundle_context.rb
@@ -1,7 +1,24 @@
 class BundleContext < ApplicationRecord
+  belongs_to :user
+
   validates :project_name, presence: true, null: false
   validates :content_structure, presence: true, null: false
   validates :bundle_dir, presence: true, null: false
   validates :staging_style_symlink, presence: true, null: false
   validates :content_metadata_creation, presence: true, null: false
+
+  enum content_structure: {
+    "simple_image" => 1,
+    "simple_book" => 2,
+    "book_as_iamge" => 3,
+    "file" => 4,
+    "smpl" => 5
+  }
+
+  enum content_metadata_creation: {
+    "default" => 1,
+    "filename" => 2,
+    "smpl" => 3
+  }
+
 end
