b7277dab34c439788703247ca159f5b106ac43bc
Sarav Shah <saravsshah@gmail.com>
2018-09-05 17:33:27 -0700

    Rename's the enums and changes validation
    For a boolean, can't use presence: true, but instead can use inclusion:
    { in: [true, false] }

--- app/models/bundle_context.rb
+++ app/models/bundle_context.rb
@@ -4,21 +4,21 @@ class BundleContext < ApplicationRecord
   validates :project_name, presence: true, null: false
   validates :content_structure, presence: true, null: false
   validates :bundle_dir, presence: true, null: false
-  validates :staging_style_symlink, presence: true, null: false
+  validates :staging_style_symlink, inclusion: { in: [ true, false ] }
   validates :content_metadata_creation, presence: true, null: false
 
   enum content_structure: {
-    "simple_image" => 1,
-    "simple_book" => 2,
-    "book_as_iamge" => 3,
-    "file" => 4,
-    "smpl" => 5
+    "simple_image_structure" => 0,
+    "simple_book_structure" => 1,
+    "book_as_iamge_structure" => 2,
+    "file_structure" => 3,
+    "smpl_structure" => 4
   }
 
   enum content_metadata_creation: {
-    "default" => 1,
-    "filename" => 2,
-    "smpl" => 3
+    "default_style" => 0,
+    "filename_style" => 1,
+    "smpl_style" => 2
   }
 
 end
