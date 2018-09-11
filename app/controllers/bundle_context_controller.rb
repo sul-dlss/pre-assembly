591929c83f6598aedff2955e2c983781a8fd0112
Jeremy Nelson <jermnelson@gmail.com>
2018-09-11 10:52:55 -0700

    BundleContext validation and tests in controller and erb
    For issue #137
    For issue #140

--- app/controllers/bundle_context_controller.rb
+++ app/controllers/bundle_context_controller.rb
@@ -1,11 +1,26 @@
 class BundleContextController < ApplicationController
-
   def index
+    @bundle_context = BundleContext.new
   end
 
   def create
-    logger.debug("BundleContextController.create called")
-    PreassemblyJob.perform_later
+    @bundle_context = BundleContext.new(bundle_context_params)
+
+    # TODO: ticket #270 user should be authenticated as saved in AR
+    user = User.create(sunet_id: 'temp')
+
+    @bundle_context.user = user
+    if @bundle_context.save
+      # TODO: ticket #267 Choose type of job (DiscoveryReport or Pre-Assembly) using params[:job_selection]
+      PreassemblyJob.perform_later
+    else
+      render :index
+    end
   end
 
+  private
+
+  def bundle_context_params
+    params.permit(:project_name, :content_structure, :content_metadata_creation, :bundle_dir)
+  end
 end
--- spec/controllers/bundle_context_controller_spec.rb
+++ spec/controllers/bundle_context_controller_spec.rb
@@ -1,8 +1,15 @@
-require 'rails_helper'
-
 RSpec.describe BundleContextController, type: :controller do
-
   context 'GET index' do
+    let(:bc) do
+      BundleContext.new(
+        project_name: "Smoke Test",
+        content_structure: "simple_image",
+        content_metadata_creation: "default",
+        bundle_dir: "spec/test_data/bundle_input_b",
+        user: user
+      )
+    end
+
     it "renders the index template" do
       get :index
       expect(response).to render_template("index")
@@ -14,14 +21,52 @@ RSpec.describe BundleContextController, type: :controller do
   end
 
   context "POST create" do
-    it "renders the create template" do
-      post :create
-      expect(response).to render_template("create")
+    context "Valid Parameters" do
+      let(:bc) { BundleContext.find_by(project_name: 'Smoke Test') }
+
+      before do
+        post :create, params: { project_name: "Smoke Test",
+                                content_structure: "simple_image",
+                                content_metadata_creation: "default",
+                                bundle_dir: "spec/test_data/bundle_input_b" }
+      end
+
+      it "passes valid params" do
+        expect(response.status).to eq(200)
+      end
+      it 'saves BundleContext in db' do
+        expect(bc).to be_an_instance_of BundleContext
+      end
+      it 'has the correct attributes' do
+        expect(bc.project_name).to eq 'Smoke Test'
+        expect(bc.content_structure).to eq "simple_image"
+        expect(bc.content_metadata_creation).to eq "default"
+        expect(bc.bundle_dir).to eq "spec/test_data/bundle_input_b"
+      end
+      it 'calls preassembly job when job_selection is Pre-Assembly Job' do
+        # TODO: test PreassemblyJob.perform_later when job_selection is "Pre-Assembly Job"
+      end
+      it 'calls discovery report job when job_selection is Discovery Report' do
+        # TODO: test DiscoveryReportJob.performat_later when job_select is "Discovery Report"
+      end
     end
-    it "is successful" do
-      post :create
-      expect(response.status).to eq(200)
+
+    context "Invalid Parameters" do
+      let(:post_create) { post :create }
+
+      let(:bc) { BundleContext.find_by(bundle_dir: "spec/test_data/bundle_input_b") }
+
+      before do
+        post :create, params: { project_name: nil,
+                                content_structure: "simple_image",
+                                content_metadata_creation: "default",
+                                bundle_dir: "spec/test_data/bundle_input_b" }
+      end
+
+      it 'passes invalid params' do
+        expect(bc).to be_nil
+        expect(post_create).to render_template(:index)
+      end
     end
   end
-
 end
