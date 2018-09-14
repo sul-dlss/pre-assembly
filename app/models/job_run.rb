7e6810319a6c4c0525d75a38355086a2d300b03a
Sarav Shah <saravsshah@gmail.com>
2018-09-14 12:18:55 -0700

    Add null: false to model validation

--- app/models/job_run.rb
+++ app/models/job_run.rb
@@ -1,7 +1,7 @@
 class JobRun < ApplicationRecord
   belongs_to :bundle_context
 
-  validates :job_type, presence: true
+  validates :job_type, presence: true, null: false
 
   enum job_type: {
     "discovery_report" => 0,
