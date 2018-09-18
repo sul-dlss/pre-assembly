aec386983a29b8934f515e56e5428961f903e653
Sarav Shah <saravsshah@gmail.com>
2018-09-18 13:23:45 -0700

    Use a plural controller name. Resource is plural, controller is singular

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, skip: :all
-  root to: "bundle_context#index"
-  resources :bundle_context
+  root to: "bundle_contexts#index"
+  resources :bundle_contexts
 end
