7a158980cfd4df74d288c1eae421c89326259af0
Sarav Shah <saravsshah@gmail.com>
2018-09-18 15:26:09 -0700

    Add Rspec tests to show JobRun persists, and fix failing tests

--- spec/controllers/bundle_contexts_controller_spec.rb
+++ spec/controllers/bundle_contexts_controller_spec.rb
@@ -1,10 +1,14 @@
-RSpec.describe BundleContextController, type: :controller do
+RSpec.describe BundleContextsController, type: :controller do
   let(:params) do
     {
-      project_name: 'Smoke Test',
-      content_structure: 'simple_image',
-      content_metadata_creation: 'default',
-      bundle_dir: 'spec/test_data/smpl_multimedia'
+      bundle_context:
+        {
+          project_name: 'Smoke Test',
+          content_structure: 'simple_image',
+          content_metadata_creation: 'default',
+          bundle_dir: 'spec/test_data/smpl_multimedia',
+          job_runs_attributes: { "0" =>{ job_type: "preassembly" }}
+        }
     }
   end
 
@@ -39,6 +43,7 @@ RSpec.describe BundleContextController, type: :controller do
         it 'passes newly created object' do
           expect(assigns(:bundle_context)).to be_a(BundleContext).and be_persisted
           expect(response).to have_http_status(200)
+          expect(response).to render_template("create")
         end
         it 'has the correct attributes' do
           bc = assigns(:bundle_context)
@@ -47,12 +52,26 @@ RSpec.describe BundleContextController, type: :controller do
           expect(bc.content_metadata_creation).to eq "default"
           expect(bc.bundle_dir).to eq "spec/test_data/smpl_multimedia"
         end
+        it "persists the JobRun" do
+          expect { post :create, params: params }.to change(JobRun, :count).by(1)
+        end
+
+        it "fails if job_type is nil" do
+          params[:bundle_context].merge!(job_runs_attributes: {"0" => { job_type: "" }})
+          expect { post :create, params: params }.not_to change(JobRun, :count)
+        end
       end
 
       context "Invalid Parameters" do
         it 'do not create objects' do
-          expect { post :create, params: params.merge(project_name: nil) }.not_to change(BundleContext, :count)
-          expect { post :create }.not_to change(BundleContext, :count)
+          params[:bundle_context].merge!(project_name: nil)
+          expect { post :create, params: params }.not_to change(BundleContext, :count)
+          expect { post :create, params: { bundle_context: {
+            project_name: '',
+            content_structure: '',
+            content_metadata_creation: '',
+            bundle_dir: ''
+          }}}.not_to change(BundleContext, :count)
         end
       end
     end
