480d65f2d35d5c44acd833682494ce183e3a5a93
Joe Atzberger <ohiocore@gmail.com>
2018-09-20 14:50:37 -0700

    Add JobRun template tests now that factories make it easy
    Also correct the template, prompted by tests!

--- spec/views/job_runs/show.html.erb_spec.rb
+++ spec/views/job_runs/show.html.erb_spec.rb
@@ -1,5 +1,10 @@
 RSpec.describe 'job_runs/show.html.erb', type: :view do
+  let(:job_run) { create(:job_run) }
+
   it 'displays a job_run' do
-    skip 'gimme factories'
+    assign(:job_run, job_run)
+    render
+    expect(rendered).to include("Discovery report \##{job_run.id}")
+    expect(rendered).to include("Output location /path/to/report")
   end
 end
