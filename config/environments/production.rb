009feea592d2ac7b2daa8e5e997439db8c047a3c
Joe Atzberger <ohiocore@gmail.com>
2018-09-21 17:03:56 -0700

    Configure activejob backend

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,9 +59,8 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "pre-assembly_#{Rails.env}"
+  # Use a real queuing backend for Active Job
+  config.active_job.queue_adapter = :resque
 
   config.action_mailer.perform_caching = false
 
