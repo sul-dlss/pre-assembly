7eb4c652bc9adf7174ff0b895af076d4ae113ef6
Joe Atzberger <ohiocore@gmail.com>
2018-09-21 21:28:22 -0700

    Remove preassembly `puts` statements and bespoke logging tricks
    The user won't be seeing the "tail" of the log anymore, so outputting
    (unreliable) predictions about the estimated time to completion does not
    find any audience.
    
    Nobody will see any `puts` statements on the other side of an async job
    behind a webserver. Therefore `log_and_show` is just `log`.
    
    Moved `smpl_manifest` assignment into initialize where it belongs.
    
    Removed a bunch of unused methods.
    
    NOTE: we may need to do better than the `@@log` class var for threadsafety.

--- app/lib/pre_assembly/logging.rb
+++ app/lib/pre_assembly/logging.rb
@@ -11,7 +11,6 @@ module PreAssembly
     LOG_FORMAT    = "%-6s -- %s -- %s\n"
     TIME_FORMAT   = "%Y-%m-%d %H:%M:%S"
 
-
     @@log       ||= Logger.new(File.join(Rails.root, 'log', "#{Rails.env}.log"))
     @@log.level   = LEVELS[:info]
 
@@ -21,28 +20,6 @@ module PreAssembly
 
     def log(msg, severity = :info)
       @@log.add Logging::LEVELS[severity], msg
-    end
-
-    def self.seconds_to_string(s)
-      # d = days, h = hours, m = minutes, s = seconds
-      m = (s / 60).floor
-      s = s % 60
-      h = (m / 60).floor
-      m = m % 60
-      d = (h / 24).floor
-      h = h % 24
-
-      output = "#{s} second#{pluralize(s)}" if (s > 0)
-      output = "#{m} minute#{pluralize(m)}, #{s} second#{pluralize(s)}" if (m > 0)
-      output = "#{h} hour#{pluralize(h)}, #{m} minute#{pluralize(m)}, #{s} second#{pluralize(s)}" if (h > 0)
-      output = "#{d} day#{pluralize(d)}, #{h} hour#{pluralize(h)}, #{m} minute#{pluralize(m)}, #{s} second#{pluralize(s)}" if (d > 0)
-
-      output
-    end
-
-    def self.pluralize number
-      return "s" unless number == 1
-      ""
     end
   end
 end
