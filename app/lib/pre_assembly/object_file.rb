fb1d76b99b10ab65cfd568baed72056270adf672
Joe Atzberger <ohiocore@gmail.com>
2018-09-24 15:12:13 -0700

    Redundant attr_accessor with both getter and setter overridden
    Only `attr_reader` and custom setter needed.

--- app/lib/pre_assembly/object_file.rb
+++ app/lib/pre_assembly/object_file.rb
@@ -1,19 +1,12 @@
 class PreAssembly::ObjectFile < Assembly::ObjectFile
-  attr_accessor(
-    :relative_path,
-    :exclude_from_content,
-    :checksum
-  )
+  attr_accessor :relative_path, :exclude_from_content
+  attr_reader :checksum
 
   def initialize(params = {})
     @path                 = params[:path]
     @relative_path        = params[:relative_path]
     self.checksum         = params[:checksum]
     @exclude_from_content = params[:exclude_from_content]
-  end
-
-  def checksum
-    @checksum
   end
 
   def checksum=(value)
