f27310e5c960e87d6ad3a2979452e953cf41f5fd
Joe Atzberger <ohiocore@gmail.com>
2018-09-26 13:57:04 -0700

    View presentation of download for completed job
    (or a message saying the job is not complete).

--- spec/views/job_runs/show.html.erb_spec.rb
+++ spec/views/job_runs/show.html.erb_spec.rb
@@ -1,15 +1,25 @@
 RSpec.describe 'job_runs/show.html.erb', type: :view do
   let(:job_run) { create(:job_run) }
 
+  before { assign(:job_run, job_run) }
+
   it 'displays a job_run' do
-    assign(:job_run, job_run)
     render
     expect(rendered).to include("Discovery report \##{job_run.id}")
-    expect(rendered).to include(job_run.output_location)
+  end
+
+  it 'with an output_location, presents a download link' do
+    render
+    expect(rendered).to include("<a href=\"/job_runs/#{job_run.id}/download\">Download</a>")
+  end
+
+  it 'without an output_location, urges user patience' do
+    job_run.output_location = nil
+    render
+    expect(rendered).to include('Job is not yet complete')
   end
 
   it 'displays user email from bundle context' do
-    assign(:job_run, job_run)
     render
     expect(rendered).to include(job_run.bundle_context.user.email)
   end
