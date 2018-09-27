4520b861e938ddd17c0c7721b14c904eef304d57
Joe Atzberger <ohiocore@gmail.com>
2018-09-27 16:18:39 -0700

    Fix Style/ClassAndModuleChildren in object_file.rb

--- app/lib/pre_assembly/object_file.rb
+++ app/lib/pre_assembly/object_file.rb
@@ -1,20 +1,22 @@
-class PreAssembly::ObjectFile < Assembly::ObjectFile
-  attr_accessor :relative_path, :exclude_from_content
-  attr_reader :checksum
+module PreAssembly
+  class ObjectFile < Assembly::ObjectFile
+    attr_accessor :relative_path, :exclude_from_content
+    attr_reader :checksum
 
-  def initialize(params = {})
-    @path                 = params[:path]
-    @relative_path        = params[:relative_path]
-    self.checksum         = params[:checksum]
-    @exclude_from_content = params[:exclude_from_content]
-  end
+    def initialize(params = {})
+      @path                 = params[:path]
+      @relative_path        = params[:relative_path]
+      self.checksum         = params[:checksum]
+      @exclude_from_content = params[:exclude_from_content]
+    end
 
-  def checksum=(value)
-    @checksum = value
-    self.provider_md5 = value # this is an attribute of the Assembly::ObjectFile class
-  end
+    def checksum=(value)
+      @checksum = value
+      self.provider_md5 = value # this is an attribute of the Assembly::ObjectFile class
+    end
 
-  def <=>(other)
-    @relative_path <=> other.relative_path
+    def <=>(other)
+      @relative_path <=> other.relative_path
+    end
   end
 end
