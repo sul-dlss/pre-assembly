ad3b0bf868d3192bb4ed085a47c7561bf46a9f83
Joe Atzberger <ohiocore@gmail.com>
2018-09-26 19:22:18 -0700

    Provide real link to JobRun in email
    Also standardizes the languages between subject and body.

--- app/mailers/job_mailer.rb
+++ app/mailers/job_mailer.rb
@@ -4,7 +4,6 @@ class JobMailer < ApplicationMailer
   def completion_email
     @job_run = params[:job_run]
     @user = @job_run.bundle_context.user
-    mail(to: @user.email, subject: 'Your pre-assembly job has completed')
+    mail(to: @user.email, subject: "Your #{@job_run.job_type.humanize} job completed")
   end
-
 end
