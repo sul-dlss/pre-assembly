edfe49e3e2eb46ac1a043a7c9803f149d069a1d6
Sarav Shah <saravsshah@gmail.com>
2018-10-01 14:50:00 -0700

    git revert, need to add this back

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :remote_user_authenticatable # We don't want other (default) Devise modules
-  has_many :bundle_contexts
+  has_many :bundle_contexts, dependent: :destroy
   validates :sunet_id, presence: true, uniqueness: true
 
   def email
