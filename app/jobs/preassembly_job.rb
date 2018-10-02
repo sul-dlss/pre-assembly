ad6ef434c9ddd156b0fc05c5b655152e4f1c4ee1
Sarav Shah <saravsshah@gmail.com>
2018-10-02 13:31:59 -0700

    these fixme and todo's are not needed anymore

--- app/jobs/preassembly_job.rb
+++ app/jobs/preassembly_job.rb
@@ -5,7 +5,6 @@ class PreassemblyJob < ApplicationJob
   def perform(job_run)
     bc = job_run.bundle_context
     bc.bundle.run_pre_assembly
-    # TODO: produce and save a run-specific log file, similar to discovery_report?
     job_run.output_location = bc.progress_log_file
     job_run.save!
   end
