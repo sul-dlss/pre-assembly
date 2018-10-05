ae6a1332eba790ac6e905c8099e5472995e8367a
Joe Atzberger <ohiocore@gmail.com>
2018-10-04 18:10:55 -0700

    Don't double-assign constants
    Otherwise, we get warnings on every spin-up:
    ```
    /Users/atz/repos/pre-assembly/config/initializers/load_config.rb:2: warning: already initialized constant ALLOWABLE_BUNDLE_DIRS
    /Users/atz/repos/pre-assembly/config/initializers/load_config.rb:1: warning: previous definition of ALLOWABLE_BUNDLE_DIRS was here
    ```

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,2 +1,2 @@
-ALLOWABLE_BUNDLE_DIRS = YAML.safe_load(ERB.new(File.read("#{Rails.root}/config/initializers/bundle_dir_roots.yml.erb")).result)[Rails.env]
-ALLOWABLE_BUNDLE_DIRS = ALLOWABLE_BUNDLE_DIRS.map { |path| path.chomp('/') }
+bundle_dir_roots = YAML.safe_load(ERB.new(File.read("#{Rails.root}/config/initializers/bundle_dir_roots.yml.erb")).result)[Rails.env]
+ALLOWABLE_BUNDLE_DIRS = bundle_dir_roots.map { |path| path.chomp('/') }
