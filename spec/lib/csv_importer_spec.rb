fb75f8a05568cec4ede76503a943e47dbbf47b36
Sarav Shah <saravsshah@gmail.com>
2018-10-08 14:18:41 -0700

    Coerce Win32 line endings and strip BOM to support windows CSVs
    Add RSpec tests for windows line ending

--- spec/lib/csv_importer_spec.rb
+++ spec/lib/csv_importer_spec.rb
@@ -12,5 +12,18 @@ RSpec.describe CsvImporter do
       expect(manifest).to all(include(*headers))
       expect(manifest.pluck(:description)).to eq([nil, '', 'yo, this is a description'])
     end
+    context 'windows manifest.csv' do
+      let(:manifest) do
+        described_class.parse_to_hash("#{Rails.root}/spec/test_data/windows_manifest/manifest.csv")
+      end
+
+      it 'loads a CSV as a hash and provides values' do
+        expect(manifest.size).to eq(7)
+        expect(manifest).to be_an(Array)
+        headers = %w[object druid]
+        expect(manifest).to all(be_an(ActiveSupport::HashWithIndifferentAccess)) # accessible w/ string and symbols
+        expect(manifest).to all(include(*headers))
+      end
+    end
   end
 end
