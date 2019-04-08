745022343481107208e3de281f0174918e9c5bfa
Peter Mangiafico <peter@mangiafico.org>
2019-04-08 10:02:40 -0700

    remove legacy deployment from master, which will not work because of changes to linked files

--- config/deploy/old_prod.rb
+++
@@ -1,8 +0,0 @@
-server 'sul-lyberservices-prod.stanford.edu', user: 'lyberadmin', roles: %w[web app db]
-set :repo_url, 'https://github.com/sul-dlss/pre-assembly.git'
-
-Capistrano::OneTimeKey.generate_one_time_key!
-set :rails_env, 'production'
-set :branch, 'v3-legacy'
-
-set :honeybadger_env, 'lyberservices-prod'
--- config/deploy/old_stage.rb
+++
@@ -1,9 +0,0 @@
-server 'sul-lyberservices-test.stanford.edu', user: 'lyberadmin', roles: %w[web app db]
-set :repo_url, 'https://github.com/sul-dlss/pre-assembly.git'
-
-Capistrano::OneTimeKey.generate_one_time_key!
-set :rails_env, 'test'
-set :deploy_to, '/home/lyberadmin/pre-assembly'
-set :branch, 'v3-legacy'
-
-set :honeybadger_env, 'lyberservices-test'
