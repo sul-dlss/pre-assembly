e9a277fb25e9b3e5ba9690b70a3be7217888177d
Justin Coyne <jcoyne@justincoyne.com>
2019-12-06 15:24:41 -0800

    Extract validator from discovery report

---
+++ app/services/object_file_validator.rb
@@ -0,0 +1,90 @@
+class ObjectFileValidator
+  def initialize(object:, bundle:)
+    @object = object
+    @bundle = bundle
+    @errors = {}
+    @counts = {}
+  end
+
+  attr_reader :errors, :counts
+
+  def validate
+    errors[:filename_no_extension] = filename_no_extension unless filename_no_extension.empty?
+    @counts = {
+      total_size: object.object_files.map(&:filesize).sum,
+      mimetypes: Hash.new(0),
+      filename_no_extension: filename_no_extension.count
+    }
+    object.object_files.each { |obj| counts[:mimetypes][obj.mimetype] += 1 } # number of files by mimetype
+    empty_files = object.object_files.count { |obj| obj.filesize == 0 }
+    errors[:empty_files] = empty_files if empty_files > 0
+
+    if using_media_manifest? # if we are using a media manifest, let's add how many files were found
+      bundle_id = File.basename(object.container)
+      if bundle_id && media.manifest[bundle_id]
+        cm_files = media.manifest[bundle_id].fetch(:files, [])
+        counts[:files_in_manifest] = cm_files.count
+        relative_paths = object.object_files.map(&:relative_path)
+        counts[:files_found] = (cm_files.pluck(:filename) & relative_paths).count
+        errors[:empty_manifest] = true unless counts[:files_in_manifest] > 0
+        errors[:files_found_mismatch] = true unless counts[:files_in_manifest] == counts[:files_found]
+      else
+        errors[:missing_media_container_name_or_manifest] = true
+      end
+    end
+
+    errors[:empty_object] = true unless counts[:total_size] > 0
+    errors[:missing_files] = true unless object_files_exist?
+    errors[:dupes] = true unless bundle.object_filenames_unique?(object)
+    errors.merge!(registration_check(object.druid))
+    self
+  end
+
+  def as_json(*)
+    { druid: object.druid.druid, errors: errors.compact, counts: counts }
+  end
+
+  private
+
+  attr_reader :object, :bundle
+
+  # Checks filesystem for expected files
+  def object_files_exist?
+    return false if object.object_files.empty?
+    object.object_files.map(&:path).all? { |path| File.readable?(path) }
+  end
+
+  def filename_no_extension
+    @filename_no_extension = object.object_files.map(&:path).select { |path| File.extname(path).empty? }
+  end
+
+  # @param [DruidTools]
+  # @return [Hash<Symbol => Boolean>] errors
+  def registration_check(druid)
+    begin
+      obj = Dor::Item.find(druid.druid)
+    rescue ActiveFedora::ObjectNotFoundError
+      return { item_not_registered: true }
+    end
+    begin
+      return { apo_empty: true } unless obj.admin_policy_object
+      {}
+    rescue ActiveFedora::ObjectNotFoundError
+      return { apo_not_registered: true }
+    end
+  rescue RuntimeError # HTTP timeout, network error, whatever
+    { dor_connection_error: true }
+  end
+
+  # @return [PreAssembly::Media]
+  def media
+    @media ||= PreAssembly::Media.new(csv_filename: bundle.bundle_context.media_manifest,
+                                      bundle_dir: bundle.bundle_dir)
+  end
+
+  # @return [Boolean]
+  def using_media_manifest?
+    bundle.content_md_creation == 'media_cm_style' &&
+      File.exist?(File.join(bundle.bundle_dir, bundle.bundle_context.media_manifest))
+  end
+end
---
+++ spec/services/object_file_validator_spec.rb
@@ -0,0 +1,59 @@
+RSpec.describe ObjectFileValidator do
+  subject(:validator) { described_class.new(object: object, bundle: bundle) }
+
+  let(:bundle) { bundle_setup(:flat_dir_images) }
+
+  describe '#validate' do
+    subject(:json) { validator.validate.as_json }
+
+    let(:object) { bundle.objects_to_process.first }
+
+    before do
+      allow(object).to receive(:pid).and_return('kk203bw3276')
+      allow(validator).to receive(:registration_check).and_return({}) # pretend everything is in Dor
+    end
+
+    it 'converts a DigtialObject to structured data (Hash)' do
+      expect(validator.validate.as_json).to match a_hash_including(
+        counts: a_hash_including(total_size: 0),
+        errors: a_hash_including(missing_files: true),
+        druid: 'druid:kk203bw3276'
+      )
+    end
+
+    context 'folders are empty' do
+      it 'adds empty_object error' do
+        expect(json).to match a_hash_including(errors: a_hash_including(empty_object: true))
+      end
+    end
+
+    context 'folders are not empty' do
+      let(:obj_file) { instance_double(PreAssembly::ObjectFile, path: 'random/path', filesize: 324, mimetype: '') }
+
+      before do
+        allow(object).to receive(:object_files).and_return([obj_file, obj_file])
+        # allow(report).to receive(:using_media_manifest?).and_return(false)
+        allow(validator).to receive(:registration_check).and_return({}) # pretend everything is in Dor
+      end
+
+      it 'does not add empty_object error' do
+        expect(json).not_to include(a_hash_including(empty_object: true))
+      end
+    end
+
+    context 'missing_media_container_name_or_manifest' do
+      let(:bundle) { bundle_setup(:media_missing) }
+      let(:obj_file) { instance_double(PreAssembly::ObjectFile, path: '', filesize: 324, mimetype: '') }
+
+      before do
+        allow(object).to receive(:object_files).and_return([obj_file, obj_file])
+        # allow(report).to receive(:using_media_manifest?).and_return(true)
+        allow(validator).to receive(:registration_check).and_return({}) # pretend everything is in Dor
+      end
+
+      it 'adds missing_media_container_name_or_manifest error' do
+        expect(json).to match a_hash_including(errors: a_hash_including(missing_media_container_name_or_manifest: true))
+      end
+    end
+  end
+end
