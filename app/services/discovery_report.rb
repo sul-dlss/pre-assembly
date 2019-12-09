sul-dlss/pre-assembly

commit e9a277fb25e9b3e5ba9690b70a3be7217888177d

Extract validator from discovery report

app/services/discovery_report.rb
@@ -35,37 +35,7 @@ class DiscoveryReport
   # @param [PreAssembly::DigitalObject]
   # @return [Hash<Symbol => Object>]
   def process_dobj(dobj)
-    errors = {}
-    filename_no_extension = dobj.object_files.map(&:path).select { |path| File.extname(path).empty? }
-    errors[:filename_no_extension] = filename_no_extension unless filename_no_extension.empty?
-    counts = {
-      total_size: dobj.object_files.map(&:filesize).sum,
-      mimetypes: Hash.new(0),
-      filename_no_extension: filename_no_extension.count
-    }
-    dobj.object_files.each { |obj| counts[:mimetypes][obj.mimetype] += 1 } # number of files by mimetype
-    empty_files = dobj.object_files.count { |obj| obj.filesize == 0 }
-    errors[:empty_files] = empty_files if empty_files > 0
-
-    if using_media_manifest? # if we are using a media manifest, let's add how many files were found
-      bundle_id = File.basename(dobj.container)
-      if bundle_id && media.manifest[bundle_id]
-        cm_files = media.manifest[bundle_id].fetch(:files, [])
-        counts[:files_in_manifest] = cm_files.count
-        relative_paths = dobj.object_files.map(&:relative_path)
-        counts[:files_found] = (cm_files.pluck(:filename) & relative_paths).count
-        errors[:empty_manifest] = true unless counts[:files_in_manifest] > 0
-        errors[:files_found_mismatch] = true unless counts[:files_in_manifest] == counts[:files_found]
-      else
-        errors[:missing_media_container_name_or_manifest] = true
-      end
-    end
-
-    errors[:empty_object] = true unless counts[:total_size] > 0
-    errors[:missing_files] = true unless dobj.object_files_exist?
-    errors[:dupes] = true unless object_filenames_unique?(dobj)
-    errors.merge!(registration_check(dobj.druid))
-    { druid: dobj.druid.druid, errors: errors.compact, counts: counts }
+    ObjectFileValidator.new(object: dobj, bundle: bundle).validate
   end
 
   # @param [DruidTools]

spec/services/discovery_report_spec.rb
@@ -47,58 +47,6 @@ RSpec.describe DiscoveryReport do
     end
   end
 
-  describe '#process_dobj' do
-    let(:dobj) { report.bundle.objects_to_process.first }
-
-    before do
-      allow(dobj).to receive(:pid).and_return('kk203bw3276')
-      allow(report).to receive(:registration_check).and_return({}) # pretend everything is in Dor
-    end
-
-    it 'converts a DigtialObject to structured data (Hash)' do
-      expect(report.process_dobj(dobj)).to match a_hash_including(
-        counts: a_hash_including(total_size: 0),
-        errors: a_hash_including(missing_files: true),
-        druid: 'druid:kk203bw3276'
-      )
-    end
-
-    context 'folders are empty' do
-      it 'adds empty_object error' do
-        expect(report.process_dobj(dobj)).to match a_hash_including(errors: a_hash_including(empty_object: true))
-      end
-    end
-
-    context 'folders are not empty' do
-      let(:obj_file) { instance_double(PreAssembly::ObjectFile, path: 'random/path', filesize: 324, mimetype: '') }
-
-      before do
-        allow(dobj).to receive(:object_files).and_return([obj_file, obj_file])
-        allow(report).to receive(:using_media_manifest?).and_return(false)
-        allow(report).to receive(:registration_check).and_return({}) # pretend everything is in Dor
-      end
-
-      it 'does not add empty_object error' do
-        expect(report.process_dobj(dobj)).not_to include(a_hash_including(empty_object: true))
-      end
-    end
-
-    context 'missing_media_container_name_or_manifest' do
-      let(:bundle) { bundle_setup(:media_missing) }
-      let(:obj_file) { instance_double(PreAssembly::ObjectFile, path: '', filesize: 324, mimetype: '') }
-
-      before do
-        allow(dobj).to receive(:object_files).and_return([obj_file, obj_file])
-        allow(report).to receive(:using_media_manifest?).and_return(true)
-        allow(report).to receive(:registration_check).and_return({}) # pretend everything is in Dor
-      end
-
-      it 'adds missing_media_container_name_or_manifest error' do
-        expect(report.process_dobj(dobj)).to match a_hash_including(errors: a_hash_including(missing_media_container_name_or_manifest: true))
-      end
-    end
-  end
-
   context 'integration test' do
     let(:bc_params) do
       {
@@ -116,11 +64,12 @@ RSpec.describe DiscoveryReport do
 
     before do
       allow(dobj).to receive(:pid).and_return('kk203bw3276')
+      allow(Dor::Item).to receive(:find).and_return(instance_double(Dor::Item, admin_policy_object: ''))
       allow(report).to receive(:registration_check).and_return({}) # pretend everything is in Dor
     end
 
     it 'process_dobj gives expected output for one dobj' do
-      expect(report.process_dobj(dobj)).to eq(
+      expect(report.process_dobj(dobj).as_json).to eq(
         druid: 'druid:kk203bw3276',
         errors: { dupes: true },
         counts: {

app/lib/pre_assembly/digital_object.rb
@@ -169,12 +169,6 @@ module PreAssembly
                                  media_manifest: media_manifest).create
     end
 
-    # Checks filesystem for expected files
-    def object_files_exist?
-      return false if object_files.empty?
-      object_files.map(&:path).all? { |path| File.readable?(path) }
-    end
-
     ####
     # Versioning for a re-accession.
     ####

app/services/object_file_validator.rb
@@ -1,0 +1,90 @@
+class ObjectFileValidator
+  def initialize(object:, bundle:)
+    @object = object
+    @bundle = bundle
+    @errors = {}
+    @counts = {}
+  end
+
+  attr_reader :errors, :counts
+
+  def validate
+    errors[:filename_no_extension] = filename_no_extension unless filename_no_extension.empty?
+    @counts = {
+      total_size: object.object_files.map(&:filesize).sum,
+      mimetypes: Hash.new(0),
+      filename_no_extension: filename_no_extension.count
+    }
+    object.object_files.each { |obj| counts[:mimetypes][obj.mimetype] += 1 } # number of files by mimetype
+    empty_files = object.object_files.count { |obj| obj.filesize == 0 }
+    errors[:empty_files] = empty_files if empty_files > 0
+
+    if using_media_manifest? # if we are using a media manifest, let's add how many files were found
+      bundle_id = File.basename(object.container)
+      if bundle_id && media.manifest[bundle_id]
+        cm_files = media.manifest[bundle_id].fetch(:files, [])
+        counts[:files_in_manifest] = cm_files.count
+        relative_paths = object.object_files.map(&:relative_path)
+        counts[:files_found] = (cm_files.pluck(:filename) & relative_paths).count
+        errors[:empty_manifest] = true unless counts[:files_in_manifest] > 0
+        errors[:files_found_mismatch] = true unless counts[:files_in_manifest] == counts[:files_found]
+      else
+        errors[:missing_media_container_name_or_manifest] = true
+      end
+    end
+
+    errors[:empty_object] = true unless counts[:total_size] > 0
+    errors[:missing_files] = true unless object_files_exist?
+    errors[:dupes] = true unless bundle.object_filenames_unique?(object)
+    errors.merge!(registration_check(object.druid))
+    self
+  end
+
+  def as_json(*)
+    { druid: object.druid.druid, errors: errors.compact, counts: counts }
+  end
+
+  private
+
+  attr_reader :object, :bundle
+
+  # Checks filesystem for expected files
+  def object_files_exist?
+    return false if object.object_files.empty?
+    object.object_files.map(&:path).all? { |path| File.readable?(path) }
+  end
+
+  def filename_no_extension
+    @filename_no_extension = object.object_files.map(&:path).select { |path| File.extname(path).empty? }
+  end
+
+  # @param [DruidTools]
+  # @return [Hash<Symbol => Boolean>] errors
+  def registration_check(druid)
+    begin
+      obj = Dor::Item.find(druid.druid)
+    rescue ActiveFedora::ObjectNotFoundError
+      return { item_not_registered: true }
+    end
+    begin
+      return { apo_empty: true } unless obj.admin_policy_object
+      {}
+    rescue ActiveFedora::ObjectNotFoundError
+      return { apo_not_registered: true }
+    end
+  rescue RuntimeError # HTTP timeout, network error, whatever
+    { dor_connection_error: true }
+  end
+
+  # @return [PreAssembly::Media]
+  def media
+    @media ||= PreAssembly::Media.new(csv_filename: bundle.bundle_context.media_manifest,
+                                      bundle_dir: bundle.bundle_dir)
+  end
+
+  # @return [Boolean]
+  def using_media_manifest?
+    bundle.content_md_creation == 'media_cm_style' &&
+      File.exist?(File.join(bundle.bundle_dir, bundle.bundle_context.media_manifest))
+  end
+end

spec/services/object_file_validator_spec.rb
@@ -1,0 +1,59 @@
+RSpec.describe ObjectFileValidator do
+  subject(:validator) { described_class.new(object: object, bundle: bundle) }
+
+  let(:bundle) { bundle_setup(:flat_dir_images) }
+
+  describe '#validate' do
+    subject(:json) { validator.validate.as_json }
+
+    let(:object) { bundle.objects_to_process.first }
+
+    before do
+      allow(object).to receive(:pid).and_return('kk203bw3276')
+      allow(validator).to receive(:registration_check).and_return({}) # pretend everything is in Dor
+    end
+
+    it 'converts a DigtialObject to structured data (Hash)' do
+      expect(validator.validate.as_json).to match a_hash_including(
+        counts: a_hash_including(total_size: 0),
+        errors: a_hash_including(missing_files: true),
+        druid: 'druid:kk203bw3276'
+      )
+    end
+
+    context 'folders are empty' do
+      it 'adds empty_object error' do
+        expect(json).to match a_hash_including(errors: a_hash_including(empty_object: true))
+      end
+    end
+
+    context 'folders are not empty' do
+      let(:obj_file) { instance_double(PreAssembly::ObjectFile, path: 'random/path', filesize: 324, mimetype: '') }
+
+      before do
+        allow(object).to receive(:object_files).and_return([obj_file, obj_file])
+        # allow(report).to receive(:using_media_manifest?).and_return(false)
+        allow(validator).to receive(:registration_check).and_return({}) # pretend everything is in Dor
+      end
+
+      it 'does not add empty_object error' do
+        expect(json).not_to include(a_hash_including(empty_object: true))
+      end
+    end
+
+    context 'missing_media_container_name_or_manifest' do
+      let(:bundle) { bundle_setup(:media_missing) }
+      let(:obj_file) { instance_double(PreAssembly::ObjectFile, path: '', filesize: 324, mimetype: '') }
+
+      before do
+        allow(object).to receive(:object_files).and_return([obj_file, obj_file])
+        # allow(report).to receive(:using_media_manifest?).and_return(true)
+        allow(validator).to receive(:registration_check).and_return({}) # pretend everything is in Dor
+      end
+
+      it 'adds missing_media_container_name_or_manifest error' do
+        expect(json).to match a_hash_including(errors: a_hash_including(missing_media_container_name_or_manifest: true))
+      end
+    end
+  end
+end

commit 14fc4c81e6fe80509aca57ebd2f7a669e9d9ed8c

Call the methods not the hash accessor
Fixes a problem I introduced in e9a277fb25e9b3e5ba9690b70a3be7217888177d
Fixes #579

app/services/discovery_report.rb
@@ -20,9 +20,9 @@ class DiscoveryReport
     return enum_for(:each_row) unless block_given?
     bundle.objects_to_process.each do |dobj|
       row = process_dobj(dobj)
-      summary[:total_size] += row[:counts][:total_size]
-      summary[:objects_with_error] += 1 unless row[:errors].empty?
-      row[:counts][:mimetypes].each { |k, v| summary[:mimetypes][k] += v }
+      summary[:total_size] += row.counts[:total_size]
+      summary[:objects_with_error] += 1 unless row.errors.empty?
+      row.counts[:mimetypes].each { |k, v| summary[:mimetypes][k] += v }
       yield row
     end
   end

spec/services/discovery_report_spec.rb
@@ -14,17 +14,17 @@ RSpec.describe DiscoveryReport do
   end
 
   describe '#each_row' do
-    let(:dobj_hash1) { { druid: '1', counts: { total_size: 1, mimetypes: { a: 1, b: 2 } }, errors: {} } }
-    let(:dobj_hash2) { { druid: '2', counts: { total_size: 2, mimetypes: { b: 3, q: 4 } }, errors: {} } }
-    let(:dobj_hash3) { { druid: '3', counts: { total_size: 3, mimetypes: { q: 9 } }, errors: { foo: true } } }
+    let(:validator1) { instance_double(ObjectFileValidator, counts: { total_size: 1, mimetypes: { a: 1, b: 2 } }, errors: {}) }
+    let(:validator2) { instance_double(ObjectFileValidator, counts: { total_size: 2, mimetypes: { b: 3, q: 4 } }, errors: {}) }
+    let(:validator3) { instance_double(ObjectFileValidator, counts: { total_size: 3, mimetypes: { q: 9 } }, errors: { foo: true }) }
 
     it 'returns an Enumerable of Hashes' do
       expect(report.each_row).to be_an(Enumerable)
     end
     it 'yields per objects_to_process, building an aggregate summary' do
-      expect(report).to receive(:process_dobj).with(1).and_return(dobj_hash1)
-      expect(report).to receive(:process_dobj).with(2).and_return(dobj_hash2)
-      expect(report).to receive(:process_dobj).with(3).and_return(dobj_hash3)
+      expect(report).to receive(:process_dobj).with(1).and_return(validator1)
+      expect(report).to receive(:process_dobj).with(2).and_return(validator2)
+      expect(report).to receive(:process_dobj).with(3).and_return(validator3)
       expect(bundle).to receive(:objects_to_process).and_return([1, 2, 3])
       report.each_row { |_r| } # no-op
       expect(report.summary).to match a_hash_including(