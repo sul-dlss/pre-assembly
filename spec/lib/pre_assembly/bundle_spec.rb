d7b816d7111fd9dbd99139bb83bb0297f9206c39
Justin Coyne <jcoyne@justincoyne.com>
2019-12-18 09:45:10 -0600

    Remove processed_pids
    It is never used outside of tests

--- spec/lib/pre_assembly/bundle_spec.rb
+++ spec/lib/pre_assembly/bundle_spec.rb
@@ -15,15 +15,12 @@ RSpec.describe PreAssembly::Bundle do
       allow(b).to receive(:log) # log statements we don't care about here
     end
 
-    it 'returns processed_pids' do
-      allow(b).to receive(:processed_pids).and_return ['druid:aa111aa1111', 'druid:bb222bb2222']
-      expect(b.run_pre_assembly).to eq ['druid:aa111aa1111', 'druid:bb222bb2222']
-    end
     it 'logs the start and finish of the run' do
       expect(b).to receive(:log).with("\nstarting run_pre_assembly(#{b.run_log_msg})")
       expect(b).to receive(:log).with("\nfinishing run_pre_assembly(#{b.run_log_msg})")
       b.run_pre_assembly
     end
+
     it 'calls process_digital_objects' do
       expect(b).to receive(:process_digital_objects)
       b.run_pre_assembly
@@ -80,14 +77,6 @@ RSpec.describe PreAssembly::Bundle do
       expect(flat_dir_images.run_log_msg).to match(/bundle_dir="#{flat_dir_images.bundle_dir}"/)
       expect(flat_dir_images.run_log_msg).to match(/assembly_staging_dir="#{Settings.assembly_staging_dir}"/)
       expect(flat_dir_images.run_log_msg).to match(/environment="test"/)
-    end
-  end
-
-  describe '#processed_pids' do
-    it 'pulls pids from digital_objects' do
-      exp_pids = [11, 22, 33]
-      flat_dir_images.digital_objects = exp_pids.map { |p| instance_double(PreAssembly::DigitalObject, pid: p) }
-      expect(flat_dir_images.processed_pids).to eq(exp_pids)
     end
   end
 
