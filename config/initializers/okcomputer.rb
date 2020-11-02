a15be3139ba72a2c0ff9ef189d99dff6725c87fa
Tony Zanella <tony.zanella@gmail.com>
2020-11-02 13:16:34 -0800

    Spot check db for data loss

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -43,6 +43,34 @@ end
 # Confirm job_output_parent_dir exists (or else jobs cannot output)
 OkComputer::Registry.register 'feature-job_output_parent_dir', DirectoryExistsCheck.new(Settings.job_output_parent_dir)
 
+# spot check tables for data loss
+class TablesHaveDataCheck < OkComputer::Check
+  def check
+    msg = [
+      User,
+      JobRun,
+      BatchContext
+    ].map { |klass| table_check(klass) }.join(' ')
+    mark_message msg
+  end
+
+  private
+
+  # @return [String] message
+  def table_check(klass)
+    # has at least 1 record
+    return "#{klass.name} has data." if klass.any?
+
+    mark_failure
+    "#{klass.name} has no data."
+  rescue => e # rubocop:disable Style/RescueStandardError
+    mark_failure
+    "#{e.class.name} received: #{e.message}."
+  end
+end
+
+OkComputer::Registry.register 'feature-tables-have-data', TablesHaveDataCheck.new
+
 # check for the right number of workers
 class WorkerCountCheck < OkComputer::Check
   def check
