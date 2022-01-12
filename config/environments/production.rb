b2a470c5727dd9cd075a5881e4fd3aaba1db49ab
Peter Mangiafico <peter@mangiafico.org>
2022-01-12 11:57:50 -0800

    add back additional config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "pre_assembly_production"
 
+  config.action_mailer.default_url_options = { host: Settings.mailer_host, protocol: 'https' } # needed by url_for() in mail templates
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
