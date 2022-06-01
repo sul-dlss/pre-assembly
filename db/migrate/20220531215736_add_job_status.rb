af452573eb7d511f87d08939fb74f71e9158251b
Peter Mangiafico <peter@mangiafico.org>
2022-06-01 16:45:49 -0700

    update migration to set all existing jobs that have log file to completed

--- db/migrate/20220531215736_add_job_status.rb
+++ db/migrate/20220531215736_add_job_status.rb
@@ -1,6 +1,12 @@
 class AddJobStatus < ActiveRecord::Migration[7.0]
-  def change
+  def up
     add_column :job_runs, :state, :string, null: false, default: 'waiting'
     add_index :job_runs, :state
+    # set all existing jobs with a finished output_location to completed
+    JobRun.where.not(output_location: nil).find_each {|j| j.update_column(:state, 'complete')}
+  end
+
+  def down
+    remove_column :job_runs, :state
   end
 end
--- spec/jobs/discovery_report_job_spec.rb
+++ spec/jobs/discovery_report_job_spec.rb
@@ -10,19 +10,30 @@ RSpec.describe DiscoveryReportJob, type: :job do
   after { FileUtils.rm(outfile) if File.exist?(outfile) } # cleanup
 
   describe '#perform' do
-    let(:jbuilder) { instance_double(Jbuilder, target!: '{"x":1}') } # mock the expensive stuff
+    context 'when success' do
+      let(:jbuilder) { instance_double(Jbuilder, target!: '{"x":1}') } # mock the expensive stuff
 
-    before { allow(job_run.to_discovery_report).to receive(:to_builder).and_return(jbuilder) }
+      before { allow(job_run.to_discovery_report).to receive(:to_builder).and_return(jbuilder) }
 
-    it 'requires param' do
-      expect { job.perform }.to raise_error(ArgumentError)
-      expect { job.perform(job_run) }.not_to raise_error
+      it 'requires param' do
+        expect { job.perform }.to raise_error(ArgumentError)
+        expect { job.perform(job_run) }.not_to raise_error
+      end
+
+      it 'writes JSON file and saves job_run.output_location' do
+        expect { job.perform(job_run) }.to change { File.exist?(outfile) }.to(true)
+        expect(job_run.reload.output_location).to eq(outfile)
+        expect(job_run).to be_complete
+      end
     end
 
-    it 'writes JSON file and saves job_run.output_location' do
-      expect { job.perform(job_run) }.to change { File.exist?(outfile) }.to(true)
-      expect(job_run.reload.output_location).to eq(outfile)
-      expect(job_run).to be_complete
+    context 'when errors' do
+      before { allow(job_run.to_discovery_report).to receive(:to_builder).and_return(StandardError) }
+
+      it 'starts report and ends in an error state' do
+        job.perform(job_run)
+        expect(job_run).to be_error
+      end
     end
   end
 end
