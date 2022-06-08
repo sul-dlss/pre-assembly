03e80027b54f4a8625a7e6bedaf3466258821efc
Peter Mangiafico <peter@mangiafico.org>
2022-06-08 13:19:03 -0700

    rescue exceptions in discovery reports, track errors on objects in discovery reports

--- app/jobs/discovery_report_job.rb
+++ app/jobs/discovery_report_job.rb
@@ -6,10 +6,15 @@ class DiscoveryReportJob < ApplicationJob
   # @param [JobRun] job_run
   def perform(job_run)
     job_run.started
-    report = job_run.to_discovery_report
-    file = File.open(report.output_path, 'w') { |f| f << report.to_builder.target! }
-    job_run.output_location = file.path # don't call report.output_path again
-    job_run.save!
-    job_run.completed
+    begin
+      report = job_run.to_discovery_report
+      file = File.open(report.output_path, 'w') { |f| f << report.to_builder.target! }
+      job_run.output_location = file.path # don't call report.output_path again
+      job_run.save!
+      job_run.completed
+    rescue StandardError => e
+      job_run.error_message = e.message
+      job_run.failed
+    end
   end
 end
--- spec/jobs/preassembly_job_spec.rb
+++ spec/jobs/preassembly_job_spec.rb
@@ -5,10 +5,11 @@ RSpec.describe PreassemblyJob, type: :job do
   let(:job_run) { create(:job_run, :preassembly) }
   let(:outfile) { 'tmp/foobar_progress.yaml' }
   let(:batch_context) { job_run.batch_context }
+  let(:batch) { job_run.batch_context.batch }
 
   before do
     allow(batch_context).to receive(:progress_log_file).and_return(outfile)
-    allow(batch_context.batch).to receive(:process_digital_objects)
+    allow(batch).to receive(:process_digital_objects)
   end
 
   after { FileUtils.rm(outfile) if File.exist?(outfile) } # cleanup
@@ -20,12 +21,26 @@ RSpec.describe PreassemblyJob, type: :job do
     end
 
     context 'when success' do
-      before { allow(batch_context.batch).to receive(:had_errors).and_return(false) }
+      before { allow(batch).to receive(:had_errors).and_return(false) }
 
       it 'calls run_pre_assembly and ends in an complete state' do
+        expect(batch).to receive(:run_pre_assembly)
         job.perform(job_run)
         expect(job_run).to be_complete
         expect(job_run.error_message).to be_nil
+      end
+    end
+
+    context 'when failed' do
+      let(:error_message) { 'something really unexpected happened' }
+      # simulate an uncaught exception while running the job
+
+      before { allow(batch).to receive(:process_digital_objects).and_raise(StandardError, error_message) }
+
+      it 'ends in a failed state with an uncaught exception, and saves error message to the database' do
+        job.perform(job_run)
+        expect(job_run).to be_failed
+        expect(job_run.error_message).to eq error_message
       end
     end
 
@@ -33,11 +48,12 @@ RSpec.describe PreassemblyJob, type: :job do
       let(:error_message) { 'something bad happened' }
 
       before do
-        allow(batch_context.batch).to receive(:had_errors).and_return(true)
-        allow(batch_context.batch).to receive(:error_message).and_return(error_message)
+        allow(batch).to receive(:had_errors).and_return(true)
+        allow(batch).to receive(:error_message).and_return(error_message)
       end
 
       it 'calls run_pre_assembly and ends in a completed with error state, and saves error message to the database' do
+        expect(batch).to receive(:run_pre_assembly)
         job.perform(job_run)
         expect(job_run).to be_complete_with_errors
         expect(job_run.error_message).to eq error_message
