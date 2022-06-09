0baaf52ff12286185f263dca8f356d12e7750dc6
Justin Coyne <jcoyne@justincoyne.com>
2022-06-09 10:06:43 -0500

    Tighten up the brand bar

--- spec/views/shared/_header_spec.rb
+++ spec/views/shared/_header_spec.rb
@@ -1,27 +1,21 @@
 # frozen_string_literal: true
 
 RSpec.describe 'shared/_header.erb' do
-  context 'should have the right links on the header' do
-    before { render template: 'shared/_header' }
+  before { render template: 'shared/_header' }
 
-    it 'Usage Instructions' do
-      expect(rendered).to include("<a class=\"nav-link\" href=\"https://github.com/sul-dlss/pre-assembly/wiki\">Usage Instructions</a>\n")
-    end
+  it 'Usage Instructions' do
+    expect(rendered).to include("<a class=\"nav-link\" href=\"https://github.com/sul-dlss/pre-assembly/wiki\">Usage Instructions</a>\n")
+  end
 
-    it 'All Jobs' do
-      expect(rendered).to include("<a class=\"nav-link\" href=\"/job_runs\">All Jobs</a>\n")
-    end
+  it 'All Jobs' do
+    expect(rendered).to include("<a class=\"nav-link\" href=\"/job_runs\">All Jobs</a>\n")
+  end
 
-    it 'Start New Projects' do
-      expect(rendered).to include("<a class=\"nav-link\" href=\"/\">Start New Project</a>\n")
-    end
+  it 'Start New Projects' do
+    expect(rendered).to include("<a class=\"nav-link\" href=\"/\">Start New Project</a>\n")
+  end
 
-    it 'All Projects' do
-      expect(rendered).to include("<a class=\"nav-link\" href=\"/batch_contexts\">All Projects</a>\n")
-    end
-
-    it 'Accessioning' do
-      expect(rendered).to include("<h1 class=\"p-4\">Accessioning</h1>\n")
-    end
+  it 'All Projects' do
+    expect(rendered).to include("<a class=\"nav-link\" href=\"/batch_contexts\">All Projects</a>\n")
   end
 end
