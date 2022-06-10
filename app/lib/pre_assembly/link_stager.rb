ca704ac01a573c6d09381b8586a6068f306553c3
Naomi Dushay <ndushay@stanford.edu>
2022-06-10 00:36:09 -0700

    add class comments for Stager classes

--- app/lib/pre_assembly/link_stager.rb
+++ app/lib/pre_assembly/link_stager.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module PreAssembly
+  # Links are used to stage objects; useful for large files (e.g. media, WARC files)
   class LinkStager
     def self.stage(source, destination)
       FileUtils.ln_s source, destination, force: true
