81210377f48bf0f308615f23c80e26fc3b02519e
Naomi Dushay <ndushay@stanford.edu>
2022-06-10 11:08:40 -0700

    assembly_directory.rb: make methods private and improve comments

--- spec/lib/pre_assembly/assembly_directory_spec.rb
+++ spec/lib/pre_assembly/assembly_directory_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe PreAssembly::AssemblyDirectory do
   describe 'druid tree' do
     it 'has the correct folders (using the contemporary style)' do
       expect(object.druid_tree_dir).to eq('tmp/assembly/gn/330/dv/6119/gn330dv6119')
-      expect(object.metadata_dir).to eq('tmp/assembly/gn/330/dv/6119/gn330dv6119/metadata')
-      expect(object.content_dir).to eq('tmp/assembly/gn/330/dv/6119/gn330dv6119/content')
+      expect(object.send(:metadata_dir)).to eq('tmp/assembly/gn/330/dv/6119/gn330dv6119/metadata')
+      expect(object.send(:content_dir)).to eq('tmp/assembly/gn/330/dv/6119/gn330dv6119/content')
     end
   end
 end
