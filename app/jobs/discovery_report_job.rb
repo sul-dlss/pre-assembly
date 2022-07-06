b6666d044fb9186942d2bfeb1a76a8e5f7d7af06
Aaron Collier <aaron.collier@stanford.edu>
2022-07-06 15:31:53 -0700

    Pretty print the JSON output of the discovery report

--- app/jobs/discovery_report_job.rb
+++ app/jobs/discovery_report_job.rb
@@ -9,7 +9,8 @@ class DiscoveryReportJob < ApplicationJob
     job_run.started
     report = job_run.to_discovery_report
     # .to_builder produces JSON report by iterating over the objects
-    file = File.open(report.output_path, 'w') { |f| f << report.to_builder.target! }
+    # Note: It is required to parse the JSON first in order to correctly pretty print it
+    file = File.open(report.output_path, 'w') { |f| f << JSON.pretty_generate(JSON.parse(report.to_builder.target!)) }
     job_run.output_location = file.path
     job_run.save!
     if report.objects_had_errors # individual objects processed had errors
