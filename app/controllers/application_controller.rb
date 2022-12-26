c00bd62d329a80f03a5cc2641e7daac7db694e26
Naomi Dushay <ndushay@stanford.edu>
2022-12-26 13:10:16 -0800

    rubocop: quieted new cops

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base # rubocop:disable Style/Documentation
   before_action :authenticate_user! # ensures that remote user is logged in locally (via database)
 end
