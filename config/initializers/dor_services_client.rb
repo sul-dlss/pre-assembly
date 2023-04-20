795d93f814b82d79bf92c8ee444428bff5e18daa
Michael J. Giarlo <mjgiarlo@stanford.edu>
2023-04-20 13:16:13 -0700

    Use lower-case settings
    Do this because it is the convention we use and also because it matches shared_configs.

--- config/initializers/dor_services_client.rb
+++ config/initializers/dor_services_client.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 # Configure dor-services-client to use the dor-services URL
-Dor::Services::Client.configure(url: Settings.DOR_SERVICES.URL,
-                                token: Settings.DOR_SERVICES.TOKEN)
+Dor::Services::Client.configure(url: Settings.dor_services.url,
+                                token: Settings.dor_services.token)
