1103cab5ffd5907ef918a307418a72848a6b806f
Peter Mangiafico <peter@mangiafico.org>
2023-09-11 15:02:02 -0700

    use turbo frames for rendering discovery report summaries and progress log files

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,12 @@ Rails.application.routes.draw do
   devise_for :users, skip: :all
   root to: 'batch_contexts#new'
   resources :batch_contexts, only: %i[create index new show]
-  resources :job_runs, only: [:create, :index, :show]
-  get 'job_runs/:id/download_log', to: 'job_runs#download_log', as: 'download_log'
-  get 'job_runs/:id/download_report', to: 'job_runs#download_report', as: 'download_report'
+  resources :job_runs, only: [:create, :index, :show] do
+    member do
+      get :download_log
+      get :download_report
+      get :discovery_report_summary
+      get :progress_log
+    end
+  end
 end
--- spec/controllers/job_runs_controller_spec.rb
+++ spec/controllers/job_runs_controller_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe JobRunsController do
 
     before do
       allow(JobRun).to receive(:find).with('123').and_return(job_run)
-      allow(job_run).to receive_messages(report_ready?: false, progress_log_file: nil)
+      allow(job_run).to receive_messages(report_ready?: false, progress_log_file_exists?: false)
     end
 
     it 'returns http success' do
@@ -63,13 +63,13 @@ RSpec.describe JobRunsController do
 
   describe '#download_log' do
     it 'before job is started, renders page with flash' do
-      allow(JobRun).to receive(:find).with('123').and_return(instance_double(JobRun, progress_log_file: nil))
+      allow(JobRun).to receive(:find).with('123').and_return(instance_double(JobRun, progress_log_file_exists?: false))
       get :download_log, params: { id: 123 }
       expect(flash[:warning]).to eq('Progress log file not available.')
     end
 
     it 'returns file attachment' do
-      job_run_double = instance_double(JobRun, progress_log_file: 'spec/fixtures/input/mock_progress_log.yaml')
+      job_run_double = instance_double(JobRun, progress_log_file: 'spec/fixtures/input/mock_progress_log.yaml', progress_log_file_exists?: true)
       allow(JobRun).to receive(:find).with('123').and_return(job_run_double)
       get :download_log, params: { id: 123 }
       expect(response).to have_http_status(:success)
@@ -102,6 +102,46 @@ RSpec.describe JobRunsController do
 
     it 'requires ID param' do
       expect { get :download_report }.to raise_error(ActionController::UrlGenerationError)
+    end
+  end
+
+  describe '#discovery_report_summary' do
+    it 'before job is started, renders page with flash' do
+      allow(JobRun).to receive(:find).with('123').and_return(instance_double(JobRun, report_ready?: false))
+      get :discovery_report_summary, params: { id: 123 }
+      expect(flash[:warning]).to eq('There is no discovery report. Please check back later.')
+    end
+
+    it 'returns http success' do
+      job_run_double = instance_double(JobRun, report_ready?: true, output_location: 'spec/fixtures/input/mock_discovery_report.json')
+      allow(JobRun).to receive(:find).with('123').and_return(job_run_double)
+      get :discovery_report_summary, params: { id: 123 }
+      expect(response).to have_http_status(:success)
+      expect(flash[:warning]).to be_nil
+    end
+
+    it 'requires ID param' do
+      expect { get :discovery_report_summary }.to raise_error(ActionController::UrlGenerationError)
+    end
+  end
+
+  describe '#progress_log' do
+    it 'before job is started, renders page with flash' do
+      allow(JobRun).to receive(:find).with('123').and_return(instance_double(JobRun, progress_log_file_exists?: false))
+      get :progress_log, params: { id: 123 }
+      expect(flash[:warning]).to eq('Progress log file not available.')
+    end
+
+    it 'returns http success' do
+      job_run_double = instance_double(JobRun, progress_log_file: 'spec/fixtures/input/mock_progress_log.yaml', progress_log_file_exists?: true)
+      allow(JobRun).to receive(:find).with('123').and_return(job_run_double)
+      get :progress_log, params: { id: 123 }
+      expect(response).to have_http_status(:success)
+      expect(flash[:warning]).to be_nil
+    end
+
+    it 'requires ID param' do
+      expect { get :progress_log }.to raise_error(ActionController::UrlGenerationError)
     end
   end
 end
