228f1c8ae91ce970fbc6f8cd865e4df8e86a4343
Justin Littman <justinlittman@stanford.edu>
2023-09-19 13:54:14 -0400

    Tweak to job run complete job to avoid race condition.

--- app/jobs/job_run_complete_job.rb
+++ app/jobs/job_run_complete_job.rb
@@ -3,6 +3,7 @@
 # Updates a JobRun if all Accessions are complete
 class JobRunCompleteJob < ApplicationJob
   def perform(job_run)
+    return if job_run.running?
     return if job_run.accessioning_complete?
     return if job_run.accessions.exists?(state: :in_progress)
 
