ad51159f3225ad4e018f13bcf422ffaf231964b2
Justin Littman <justinlittman@stanford.edu>
2023-09-25 15:29:08 -0400

    Remove error has separate job run state.
    closes #1301

--- app/jobs/discovery_report_job.rb
+++ app/jobs/discovery_report_job.rb
@@ -11,12 +11,8 @@ class DiscoveryReportJob < ApplicationJob
     file = File.open(report.output_path, 'w') { |f| f << JSON.pretty_generate(JSON.parse(report.to_builder.target!)) }
     job_run.output_location = file.path
     job_run.save!
-    if report.objects_had_errors # individual objects processed had errors
-      job_run.error_message = report.error_message
-      job_run.completed_with_errors
-    else
-      job_run.completed
-    end
+    job_run.error_message = report.objects_had_errors ? report.error_message : nil
+    job_run.completed
   rescue StandardError => e # catch any error preventing the whole job from running (e.g. bad header in csv)
     job_run.error_message = e.exception
     job_run.failed
