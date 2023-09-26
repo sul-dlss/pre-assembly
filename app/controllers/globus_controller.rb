dda6257bbaa22fd2374e1ec8168f915f28bd2e2c
Laura Wrubel <lwrubel@stanford.edu>
2023-09-26 16:52:42 -0400

    Set up globus directory

--- app/controllers/globus_controller.rb
+++ app/controllers/globus_controller.rb
@@ -8,12 +8,28 @@ class GlobusController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    user = current_user
-    dest = GlobusDestination.create(user:)
-    # TODO: creat the Globus destination!
+    @user = current_user
+    @dest = GlobusDestination.create(user: @user)
+    create_globus_endpoint
+
     render status_code: :created, json: {
-      url: dest.url,
-      location: dest.staging_location
+      url: @dest.url,
+      location: @dest.staging_location
     }
+  end
+
+  private
+
+  def create_globus_endpoint
+    return true if test_mode?
+
+    user_id = "#{@user.sunet_id}@stanford.edu"
+    success = GlobusClient.mkdir(path: @dest.destination_path, user_id:)
+    raise "Error creating globus endpoint for destination #{@dest.id} for #{user_id}" unless success
+  end
+
+  # simulate globus calls in development if settings indicate we should for testing
+  def test_mode?
+    Settings.globus.test_mode && Rails.env.development?
   end
 end
