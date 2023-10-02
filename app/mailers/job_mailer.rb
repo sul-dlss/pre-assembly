3b3b7333048023d8ef98688a6df584f9ff686692
Peter Mangiafico <peter@mangiafico.org>
2023-10-02 16:42:46 -0700

    change subject lines of completion emails

--- app/mailers/job_mailer.rb
+++ app/mailers/job_mailer.rb
@@ -6,9 +6,15 @@ class JobMailer < ApplicationMailer
   def completion_email
     @job_run = params[:job_run]
     @user = @job_run.batch_context.user
+
+    # This email is sent when either (1) a discovery report completes, or (2) a preassembly job ends with errors
+    # If the preassembly job completes with no errors (and items are accessioning), a separate email is instead sent
+    # when accessioning is complete.
+    @subject = "[#{@job_run.batch_context.project_name}] Your #{@job_run.job_type.humanize} job "
+    @subject += @job_run.job_type == 'preassembly' ? 'encountered an error' : 'completed'
     mail(
       to: @user.email,
-      subject: "[#{@job_run.batch_context.project_name}] Your #{@job_run.job_type.humanize} job completed"
+      subject: @subject
     )
   end
 
@@ -19,7 +25,7 @@ class JobMailer < ApplicationMailer
     @failed_accessions = @job_run.accessions.where(state: 'failed')
     mail(
       to: @user.email,
-      subject: "[#{@job_run.batch_context.project_name}] Accessioning completed"
+      subject: "[#{@job_run.batch_context.project_name}] Job completed"
     )
   end
 end
