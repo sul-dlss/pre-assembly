214fe77a4550353a59ec62113a64bd76c5d1cf1c
Peter Mangiafico <peter@mangiafico.org>
2023-10-13 10:38:51 -0700

    add additional tests

--- app/services/globus_cleanup.rb
+++ app/services/globus_cleanup.rb
@@ -14,9 +14,6 @@ class GlobusCleanup
       next unless dest.batch_context&.accessioning_complete?
 
       cleanup_destination(dest)
-      Rails.logger.info("GlobusCleanup done for batch_context #{dest.batch_context.id}, globus_destination #{dest.id})")
-    rescue StandardError => e # catch any "Access rule not found" errors from Globus
-      Honeybadger.notify(e, context: { message: 'GlobusCleanup failed', globus_destination_id: dest.id, batch_context_id: dest.batch_context.id })
     end
   end
 
@@ -26,9 +23,6 @@ class GlobusCleanup
       next if dest.batch_context.present?
 
       cleanup_destination(dest)
-      Rails.logger.info("GlobusCleanup done for globus_destination #{dest.id})")
-    rescue StandardError => e # catch any "Access rule not found" errors from Globus
-      Honeybadger.notify(e, context: { message: 'GlobusCleanup failed', globus_destination_id: dest.id })
     end
   end
 
@@ -37,5 +31,8 @@ class GlobusCleanup
     user_id = "#{dest.user.sunet_id}@stanford.edu"
     GlobusClient.delete_access_rule(path: dest.destination_path, user_id:)
     dest.update!(deleted_at: Time.zone.now)
+    Rails.logger.info("GlobusCleanup done for globus_destination #{dest.id})")
+  rescue StandardError => e # catch any "Access rule not found" errors from Globus
+    Honeybadger.notify(e, context: { message: 'GlobusCleanup failed', globus_destination_id: dest.id })
   end
 end
