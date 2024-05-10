41c72aa3e2f7fb9e0ae4564181e7fb5c15d12b3f
dnoneill <noneill@stanford.edu>
2024-05-10 13:24:53 -0400

    update form based on selected item

--- app/javascript/controllers/ocr_controller.js
+++ app/javascript/controllers/ocr_controller.js
@@ -2,30 +2,35 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-  static targets = ['contentStructure', 'ocrSettingsImage', 'ocrSettingsDocument',
-    'manuallyCorrectedOcrImage', 'runOcrImage', 'manuallyCorrectedOcrDocument', 'runOcrDocument']
+  static targets = ['contentStructure', 'ocrSettings',
+    'manuallyCorrectedOcr', 'runOcr']
 
   connect () {
-    this.changed()
+    this.changed();
   }
 
-  changed () {
-    this.ocrSettingsImageTarget.hidden = true
-    this.ocrSettingsDocumentTarget.hidden = true
+  selected (){
+    return this.contentStructureTarget.options[this.contentStructureTarget.selectedIndex].label
+  }
 
-    if (this.contentStructureTarget.value == 'simple_image') {
-        this.ocrSettingsImageTarget.hidden = false
-        this.manuallyCorrectedOcrImageTarget.disabled = false
-        this.runOcrImageTarget.disabled = false
-        this.manuallyCorrectedOcrDocumentTarget.disabled = true
-        this.runOcrDocumentTarget.disabled = true
-    }
-    else if (this.contentStructureTarget.value == 'document') {
-        this.ocrSettingsDocumentTarget.hidden = false
-        this.manuallyCorrectedOcrDocumentTarget.disabled = false
-        this.runOcrDocumentTarget.disabled = false
-        this.manuallyCorrectedOcrImageTarget.disabled = true
-        this.runOcrImageTarget.disabled = true
+  labelManuallyCorrected() {
+    return `Do the ${this.selected()}(s) have pre-existing OCR that has been manually reviewed/corrected for accessibility?`
+  }
+
+  labelRunOcr() {
+    return `Would you like to run SDR/ABBYY OCR on your ${this.selected()}(s)?`
+  }
+
+  ocrAllowed() {
+    return ['simple_image', 'document']
+  }
+  changed () {
+    if (this.ocrAllowed().indexOf(this.contentStructureTarget.value) > -1) {
+      this.ocrSettingsTarget.hidden = false;
+      this.runOcrTarget.querySelector('legend').innerHTML = this.labelRunOcr();
+      this.manuallyCorrectedOcrTarget.querySelector('legend').innerHTML = this.labelManuallyCorrected();
+    } else {
+      this.ocrSettingsTarget.hidden = true;
     }
   }
 
