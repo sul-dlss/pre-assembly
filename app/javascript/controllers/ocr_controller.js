5ce7f71c1d44e23ddc6a87dbde56936787efec28
Peter Mangiafico <peter@mangiafico.org>
2024-05-13 16:36:53 -0700

    add additional language and ocr options to pre-assembly form

--- app/javascript/controllers/ocr_controller.js
+++ app/javascript/controllers/ocr_controller.js
@@ -3,35 +3,80 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   static targets = ['contentStructure', 'ocrSettings',
-    'manuallyCorrectedOcr', 'runOcr']
+    'manuallyCorrectedOcr', 'runOcr', 'manuallyCorrectedOcrOptions', 'runOcrOptions', 'runOcrDocumentNotes']
 
   connect () {
-    this.changed();
+    this.content_structure_changed();
   }
 
-  selected (){
-    return this.contentStructureTarget.options[this.contentStructureTarget.selectedIndex].label
+  selected_content_structure() {
+    return this.contentStructureTarget.options[this.contentStructureTarget.selectedIndex]
   }
 
-  labelManuallyCorrected() {
-    return `Do the ${this.selected()}(s) have pre-existing OCR that has been manually reviewed/corrected for accessibility?`
+  labelImagesManuallyCorrected() {
+    return `Do your images have pre-existing OCR that has been manually reviewed/corrected for accessibility?`
+  }
+
+  labelDocumentsManuallyCorrected() {
+    return `Have you manually reviewed/corrected your document(s) for WCAG/PDFUA compliance?`
   }
 
   labelRunOcr() {
-    return `Would you like to run SDR/ABBYY OCR on your ${this.selected()}(s)?`
+    return `Would you like to run SDR/ABBYY OCR on your ${this.selected_content_structure().label}(s)?`
   }
 
+  // list of content structures that are allowed to run OCR
   ocrAllowed() {
     return ['simple_image', 'document']
   }
-  changed () {
-    if (this.ocrAllowed().indexOf(this.contentStructureTarget.value) > -1) {
-      this.ocrSettingsTarget.hidden = false;
-      this.runOcrTarget.querySelector('legend').innerHTML = this.labelRunOcr();
-      this.manuallyCorrectedOcrTarget.querySelector('legend').innerHTML = this.labelManuallyCorrected();
-    } else {
-      this.ocrSettingsTarget.hidden = true;
+
+  // if the user indicates they are providing OCR and have reviewed it, hide the option to run SDR OCR
+  manually_corrected_ocr_changed() {
+    if (this.manuallyCorrectedOcrTarget.querySelector('input[type="radio"]:checked').value == 'true')
+    {
+      this.runOcrTarget.hidden = true
+    }
+    else
+    {
+      this.runOcrTarget.hidden = false
     }
   }
 
+  // if the user indicates they want to runn SDR OCR, show any relevant notes/warnings
+  run_ocr_changed() {
+    if (this.runOcrTarget.querySelector('input[type="radio"]:checked').value == 'true' && this.selected_content_structure().value == 'document')
+    {
+      this.runOcrDocumentNotesTarget.hidden = false
+    }
+    else
+    {
+      this.runOcrDocumentNotesTarget.hidden = true
+    }
+  }
+
+  content_structure_changed () {
+    // Hide the OCR settings if the selected content structure is not in the list of allowed content structures
+    if (this.ocrAllowed().indexOf(this.contentStructureTarget.value) < 0) {
+      this.ocrSettingsTarget.hidden = true
+      return
+    }
+
+    // Show the OCR settings if the selected content structure is in the list of allowed content structures
+    this.ocrSettingsTarget.hidden = false
+
+    // Set the OCR settings label
+    this.runOcrTarget.querySelector('legend').innerHTML = this.labelRunOcr()
+
+    // Set specific OCR labels/options based on the content structure
+
+    if (this.selected_content_structure().value == 'simple_image') {
+      this.manuallyCorrectedOcrTarget.querySelector('legend').innerHTML = this.labelImagesManuallyCorrected()
+      this.manuallyCorrectedOcrOptionsTargets[1].labels[0].innerHTML = 'No' // the "No" option label for manually corrected image OCR
+    }
+
+    if (this.selected_content_structure().value == 'document') {
+      this.manuallyCorrectedOcrTarget.querySelector('legend').innerHTML = this.labelDocumentsManuallyCorrected()
+      this.manuallyCorrectedOcrOptionsTargets[1].labels[0].innerHTML = "No/Don't Know" // the "No" option label for manually corrected document OCR
+    }
+  }
 }
