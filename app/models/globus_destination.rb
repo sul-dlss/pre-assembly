4fa0c6aba6cd82917136f2a3ed6eb2ef1e8141bf
Michael J. Giarlo <mjgiarlo@stanford.edu>
2026-02-13 13:21:20 -0800

    Require CGI library to load #parse method
    Fixes https://app.honeybadger.io/projects/52900/faults/127476358

--- app/models/globus_destination.rb
+++ app/models/globus_destination.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'cgi'
+
 # Model representing a Globus destination that can be used as a staging location.
 class GlobusDestination < ApplicationRecord
   belongs_to :batch_context, optional: true
